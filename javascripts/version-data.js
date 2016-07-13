b35041551fd7b2974209c8af183febf78d4d19f7
Seth Clydesdale <sethc1995@gmail.com>
2016-07-13 15:33:35 -0400

    Update version-data.js

--- javascripts/version-data.js
+++ javascripts/version-data.js
@@ -1,5 +1,8 @@
+/* ************************************************
+** DO NOT MODIFY OR DISABLE THIS JAVASCRIPT.
+** IT IS ESSENTIAL FOR UPDATING YOUR FORUM THEME.
+** FORUMACTIF EDGE (https://github.com/SethClydesdale/forumactif-edge)
+**************************************************/
 window.forumactif_edge_version_data = [
-  '1.0.0-beta',
-  '1.0.0-beta.1',
-  '1.0.0-beta.2'
+  '1.0.0-beta'
 ];

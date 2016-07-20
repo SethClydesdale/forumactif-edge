6e3750355fb406c21235939e262ea83da2a8ef51
Seth Clydesdale <sethc1995@gmail.com>
2016-07-20 13:21:22 -0400

    Create Français.js

---
+++ lang/Français.js
@@ -0,0 +1,124 @@
+// French Translation
+
+/*
+
+  REQUIRES TRANSLATION :
+
+  # CSS
+  - record_users
+  - chatbox_tip
+  - chatbox_no_away
+  - chatbox_no_online
+
+  # JAVASCRIPTS
+  - widget_menu_title
+  - widget_menu_tooltip
+  - select_theme
+  - random_theme
+  - default_theme
+  - quick_nav_title
+  - quick_nav_tooltip
+  - quick_nav_notif
+  - tool_copy_url_tooltip
+  - tool_copy_url_prompt
+  - tool_admin_panel
+  - tool_to_top
+  - tool_to_bottom
+  - img_resizer_full_size
+  - img_resizer_enlarge
+  - img_resizer_reduce
+  - img_resizer_download
+  - img_resizer_tooltip
+  - fae_update_found
+
+  # TEMPLATES
+  - back_to_top
+  - fae_feedback
+  - fae_feedback_tooltip
+
+*/
+
+FAE.lang = {
+
+  css : {
+    offline : 'HORS LIGNE',
+    online : 'EN LIGNE',
+    total_posts : 'Total de messages',
+    total_topics : 'Total de sujets',
+    total_users : 'Total de membres',
+    record_users : 'Most Members Online',
+    board_age : 'Âge du forum (en journées)',
+    newest_member : 'Nouveau Membre',
+    hidebox : 'Caché:',
+    chatbox_tip : 'Hover over to see who is online.',
+    chatbox_no_away : 'No members are currently away.',
+    chatbox_no_online : 'No members are currently online.',
+  },
+
+
+  images : {
+
+    buttons : {
+      i_post : 'http://i35.servimg.com/u/f35/18/21/41/30/new-fr10.png?color=primary&size=m',
+      i_reply : 'http://i35.servimg.com/u/f35/18/21/41/30/post-r10.png?color=primary&size=m',
+      i_reply_locked : 'http://i35.servimg.com/u/f35/18/21/41/30/locked10.png?color=secondary&size=m',
+      i_icon_quote : 'http://i35.servimg.com/u/f35/18/21/41/30/quote-10.png?color=primary&size=s',
+      i_icon_multiquote_off : 'http://i35.servimg.com/u/f35/18/21/41/30/multi-10.png?color=primary&size=s',
+      i_icon_multiquote_on : 'http://i35.servimg.com/u/f35/18/21/41/30/multi-10.png?color=secondary&size=s',
+      i_icon_thanks_off : 'http://i35.servimg.com/u/f35/18/21/41/30/thanks10.png?color=primary&size=s',
+      i_icon_thanks_on : 'http://i35.servimg.com/u/f35/18/21/41/30/thanks10.png?color=secondary&size=s',
+      i_icon_edit : 'http://i35.servimg.com/u/f35/18/21/41/30/edit-f10.png?color=primary&size=s',
+      i_msg_newpost : 'http://i35.servimg.com/u/f35/18/21/41/30/new-fr10.png?color=primary&size=m'
+    },
+
+    gallery : {
+      upload_pic : 'http://i35.servimg.com/u/f35/18/21/41/30/upload10.png?color=primary&size=m'
+    }
+
+  },
+
+
+  javascripts : {
+
+    '[FA EDGE] ALL.JS' : {
+      widget_menu_title : 'Widget Menu',
+      widget_menu_tooltip : 'Toggle widget menu',
+
+      select_theme : 'Select a theme',
+      random_theme : 'Random theme',
+      default_theme : 'Default theme',
+
+      toolbar_search_placeholder : 'Rechercher...',
+
+      quick_nav_title : 'Quick Navigation',
+      quick_nav_tooltip : 'Toggle quick navigation',
+      quick_nav_notif : 'Notifications',
+      tool_copy_url_tooltip : 'Copy BBCode URL',
+      tool_copy_url_prompt : 'Copy the BBCode URL below. (CTRL+C)',
+      tool_admin_panel : 'Admin Panel',
+      tool_to_top : 'Top of page',
+      tool_to_bottom : 'Bottom of page',
+
+      img_resizer_full_size : 'Show full size',
+      img_resizer_enlarge : 'Enlarge image',
+      img_resizer_reduce : 'Reduce image',
+      img_resizer_download : 'Download image',
+      img_resizer_tooltip : 'Click to view full image',
+
+      fae_update_found : 'An update has been found for Forumactif Edge. When you\'re ready, please proceed to your FAE Control Panel and click "Check for updates" to begin the update process.'
+    }
+
+  },
+
+
+  templates : {
+
+    'overall_footer_end.html' : {
+      back_to_top : 'Back to top',
+      fae_feedback : 'Submit Feedback',
+      fae_feedback_tooltip : 'Help improve Forumactif Edge'
+    }
+
+  }
+
+};

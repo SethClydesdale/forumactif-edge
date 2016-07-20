eb7234b99ea12e6227b9313e951c47e9fa9d34b0
Seth Clydesdale <sethc1995@gmail.com>
2016-07-20 11:50:13 -0400

    Create English.js

---
+++ lang/English.js
@@ -0,0 +1,87 @@
+// English Translation
+FAE.lang = {
+
+  css : {
+    offline : 'OFFLINE',
+    online : 'ONLINE',
+    total_posts : 'Total Posts',
+    total_topics : 'Total Topics',
+    total_users : 'Total Members',
+    record_users : 'Most Members Online',
+    board_age : 'Forum Age (days)',
+    newest_member : 'Newest Member',
+    hidebox : 'Hidden:',
+    chatbox_tip : 'Hover over to see who is online.',
+    chatbox_no_away : 'No members are currently away.',
+    chatbox_no_online : 'No members are currently online.',
+  },
+
+
+  images : {
+
+    buttons : {
+      i_post : 'http://i86.servimg.com/u/f86/18/21/41/30/new-to11.png?color=primary&size=m',
+      i_reply : 'http://i86.servimg.com/u/f86/18/21/41/30/post-r10.png?color=primary&size=m',
+      i_reply_locked : 'http://i86.servimg.com/u/f86/18/21/41/30/locked11.png?color=secondary&size=m',
+      i_icon_quote : 'http://i86.servimg.com/u/f86/18/21/41/30/quote-12.png?color=primary&size=s',
+      i_icon_multiquote_off : 'http://i86.servimg.com/u/f86/18/21/41/30/multi-10.png?color=primary&size=s',
+      i_icon_multiquote_on : 'http://i86.servimg.com/u/f86/18/21/41/30/multi-10.png?color=secondary&size=s',
+      i_icon_thanks_off : 'http://i86.servimg.com/u/f86/18/21/41/30/thanks11.png?color=primary&size=s',
+      i_icon_thanks_on : 'http://i86.servimg.com/u/f86/18/21/41/30/thanks11.png?color=secondary&size=s',
+      i_icon_edit : 'http://i86.servimg.com/u/f86/18/21/41/30/edit-e11.png?color=primary&size=s',
+      i_icon_delete : 'http://i86.servimg.com/u/f86/18/21/41/30/delete10.png?color=primary&size=s',
+      i_icon_ip : 'http://i86.servimg.com/u/f86/18/21/41/30/ip-f10.png?color=primary&size=s',
+      i_msg_newpost : 'http://i86.servimg.com/u/f86/18/21/41/30/new-pm10.png?color=primary&size=m'
+    },
+
+    gallery : {
+      upload_pic : 'http://i86.servimg.com/u/f86/18/21/41/30/upload10.png?color=primary&size=m'
+    }
+
+  },
+
+
+  javascripts : {
+
+    '[FA EDGE] ALL.JS' : {
+      widget_menu_title : 'Widget Menu',
+      widget_menu_tooltip : 'Toggle widget menu',
+
+      select_theme : 'Select a theme',
+      random_theme : 'Random theme',
+      default_theme : 'Default theme',
+
+      toolbar_search_placeholder : 'Search...',
+
+      quick_nav_title : 'Quick Navigation',
+      quick_nav_tooltip : 'Toggle quick navigation',
+      quick_nav_notif : 'Notifications',
+      tool_copy_url_tooltip : 'Copy BBCode URL',
+      tool_copy_url_prompt : 'Copy the BBCode URL below. (CTRL+C)',
+      tool_admin_panel : 'Admin Panel',
+      tool_to_top : 'Top of page',
+      tool_to_bottom : 'Bottom of page',
+
+      img_resizer_full_size : 'Show full size',
+      img_resizer_enlarge : 'Enlarge image',
+      img_resizer_reduce : 'Reduce image',
+      img_resizer_download : 'Download image',
+      img_resizer_tooltip : 'Click to view full image',
+
+      fae_update_found : 'An update has been found for Forumactif Edge. When you\'re ready, please proceed to your FAE Control Panel and click "Check for updates" to begin the update process.'
+    }
+
+  },
+
+
+  templates : {
+
+    'overall_footer_end.html' : {
+      back_to_top : 'Back to top',
+      fae_feedback : 'Submit Feedback',
+      fae_feedback_tooltip : 'Help improve Forumactif Edge'
+    }
+
+  }
+
+};

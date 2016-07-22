48975cae38e7154d0e3124ec911caa257e0afc60
Seth Clydesdale <sethc1995@gmail.com>
2016-07-22 15:40:32 -0400

    Create Greek.js
    - Added Greek translation by [Luffy](http://fmdesign.forumotion.com/t710-forumactif-edge-greek-translation#14037)

---
+++ lang/Greek.js
@@ -0,0 +1,121 @@
+// Greek Translation
+// Translation by Luffy. (http://fmdesign.forumotion.com/t710-forumactif-edge-greek-translation#14037) Thanks!!
+FAE.lang = {
+
+  language : 'Ελληνικά',
+
+  css : {
+    offline : 'ΑΠΟΣΥΝΔΕΔΕΜΕΝΟΣ',
+    online : 'ΣΥΝΔΕΔΕΜΕΝΟΣ',
+    total_posts : 'Συνολικές Δημοσιεύσεις',
+    total_topics : 'Συνολικά Θέματα',
+    total_users : 'Συνολικά Μέλη',
+    record_users : 'Ρεκόρ Ταυτόχρονων Συνδέσεων',
+    board_age : 'Ηλικία Φόρουμ (ημέρες)',
+    newest_member : 'Νεότερο Μέλος',
+    hidebox : 'Κρυφό Περιεχόμενο:',
+    chatbox_tip : 'Για να δείτε τις λίστες περάστε το ποντίκι σας πάνω από εδώ.',
+    chatbox_no_away : 'Κανένα μέλος δεν είναι Απών..',
+    chatbox_no_online : 'Κανένα μέλος δεν είναι Συνδεδεμένο.',
+  },
+
+
+  images : {
+
+    buttons : {
+      i_post : 'http://i35.servimg.com/u/f35/18/45/41/65/new-el10.png?color=primary&size=m',
+      i_reply : 'http://i35.servimg.com/u/f35/18/45/41/65/post-r10.png?color=primary&size=m',
+      i_reply_locked : 'http://i35.servimg.com/u/f35/18/45/41/65/locked11.png?color=secondary&size=m',
+      i_icon_quote : 'http://i35.servimg.com/u/f35/18/45/41/65/quote-10.png?color=primary&size=s',
+      i_icon_multiquote_off : 'http://i35.servimg.com/u/f35/18/45/41/65/multi-10.png?color=primary&size=s',
+      i_icon_multiquote_on : 'http://i35.servimg.com/u/f35/18/45/41/65/multi-10.png?color=secondary&size=s',
+      i_icon_thanks_off : 'http://i35.servimg.com/u/f35/18/45/41/65/thanks10.png?color=primary&size=s',
+      i_icon_thanks_on : 'http://i35.servimg.com/u/f35/18/45/41/65/thanks10.png?color=secondary&size=s',
+      i_icon_edit : 'http://i35.servimg.com/u/f35/18/45/41/65/edit-e10.png?color=primary&size=s',
+      i_icon_delete : 'http://i86.servimg.com/u/f86/18/21/41/30/delete10.png?color=primary&size=s',
+      i_icon_ip : 'http://i86.servimg.com/u/f86/18/21/41/30/ip-f10.png?color=primary&size=s',
+      i_icon_report : 'http://i86.servimg.com/u/f86/18/21/41/30/report10.png?color=primary&size=s',
+      i_icon_report_new : 'http://i86.servimg.com/u/f86/18/21/41/30/report10.png?color=secondary&size=s',
+      i_icon_report_locked : 'http://i86.servimg.com/u/f86/18/21/41/30/report12.png?color=secondary&size=s',
+      i_icon_lock_report : 'http://i86.servimg.com/u/f86/18/21/41/30/report12.png?color=primary&size=s',
+      i_icon_unlock_report : 'http://i86.servimg.com/u/f86/18/21/41/30/report13.png?color=primary&size=s',
+      i_icon_search : FAE.eGIF,
+      i_icon_profile : 'http://i86.servimg.com/u/f86/18/21/41/30/profil12.png?color=primary&size=s',
+      i_icon_www : 'http://i86.servimg.com/u/f86/18/21/41/30/websit10.png?color=primary&size=s',
+      i_icon_email : 'http://i86.servimg.com/u/f86/18/21/41/30/email-10.png?color=primary&size=s',
+      i_icon_pm : 'http://i86.servimg.com/u/f86/18/21/41/30/pm-f11.png?color=primary&size=s',
+      i_icon_fb : 'http://i86.servimg.com/u/f86/18/21/41/30/facebo11.png?color=primary&size=s',
+      i_icon_twitter : 'http://i86.servimg.com/u/f86/18/21/41/30/twitte10.png?color=primary&size=s',
+      i_icon_pinterest : 'http://i86.servimg.com/u/f86/18/21/41/30/pinter10.png?color=primary&size=s',
+      i_icon_aim : 'http://i86.servimg.com/u/f86/18/21/41/30/aim-f10.png?color=primary&size=s',
+      i_icon_icq_add : 'http://i86.servimg.com/u/f86/18/21/41/30/icq-f10.png?color=primary&size=s',
+      i_icon_msnm : 'http://i86.servimg.com/u/f86/18/21/41/30/msn-f10.png?color=primary&size=s',
+      i_icon_yim : 'http://i86.servimg.com/u/f86/18/21/41/30/yahoo-10.png?color=primary&size=s',
+      i_icon_skype : 'http://i86.servimg.com/u/f86/18/21/41/30/skype-10.png?color=primary&size=s',
+      i_icon_online : FAE.eGIF,
+      i_msg_newpost : 'http://i35.servimg.com/u/f35/18/45/41/65/new-pm10.png?color=primary&size=m',
+      i_msg_inbox : FAE.eGIF,
+      i_msg_sentbox : FAE.eGIF,
+      i_msg_outbox : FAE.eGIF,
+      i_msg_savebox : FAE.eGIF,
+      i_icon_ajax_edit : 'http://i86.servimg.com/u/f86/18/21/41/30/edit-f10.png',
+      i_icon_ajax_valid : 'http://i86.servimg.com/u/f86/18/21/41/30/valid-10.png',
+      i_icon_attachment_see : 'http://i86.servimg.com/u/f86/18/21/41/30/show-a10.png',
+      i_icon_attachment_download : 'http://i86.servimg.com/u/f86/18/21/41/30/downlo10.png',
+
+      // DO NOT TRANSLATE
+      page : 'buttons',
+      submit : 'Save'
+    },
+
+    gallery : {
+      upload_pic : 'http://i35.servimg.com/u/f35/18/45/41/65/upload10.png?color=primary&size=m'
+    }
+
+  },
+
+
+  javascripts : {
+
+    '[FA EDGE] ALL.JS' : {
+      widget_menu_title : 'Μενού Widget',
+      widget_menu_tooltip : 'Κλείσιμο Μενού Widget',
+
+      select_theme : 'Επιλογή Θέματος',
+      random_theme : 'Τυχαίο Θέμα',
+      default_theme : 'Εξ\' ορισμού Θέμα',
+
+      toolbar_search_placeholder : 'Αναζήτηση...',
+
+      quick_nav_title : 'Γρήγορη Πλοήγηση',
+      quick_nav_tooltip : 'Κλείσιμο Γρήγορης Πλοήγησης',
+      quick_nav_notif : 'Ειδοποιήσεις',
+      tool_copy_url_tooltip : 'Αντιγραφή συνδέσμου BBCode',
+      tool_copy_url_prompt : 'Αντιγραφή του συνδέσμου BBCode πιο κάτω. (CTRL+C)',
+      tool_admin_panel : 'Πίνακας Διαχείρισης',
+      tool_to_top : 'Πάνω μέρος της σελίδας',
+      tool_to_bottom : 'Κάτω μέρος της σελίδας',
+
+      img_resizer_full_size : 'Πλήρες μέγεθος',
+      img_resizer_enlarge : 'Μεγένθυνση',
+      img_resizer_reduce : 'Σμίκρυνση',
+      img_resizer_download : 'Κατεβάστε',
+      img_resizer_tooltip : 'Πατήστε εδώ για να δείτε την εικόνα',
+
+      fae_update_found : 'Μία αναβάθμιση βρέθηκε για το Forumactif Edge. Όταν είστε σε θέση, παρακαλούμε προχωρήστε στον Πίνακα Ελέγχου FAE και πατήστε το κουμπί "Check for updates" για την έναρξη της αναβάθμισης.'
+    }
+
+  },
+
+
+  templates : {
+
+    'overall_footer_end.html' : {
+      back_to_top : 'Επιστροφή στην κορυφή',
+      fae_feedback : 'Αποστολή Πρότασης Βελτίωσης',
+      fae_feedback_tooltip : 'Βοηθήστε στην βελτίωση του Forumactif Edge'
+    }
+
+  }
+
+};

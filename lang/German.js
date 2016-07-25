8917ef64b84b25b99b86599bb754fefecf7e827b
Seth Clydesdale <sethc1995@gmail.com>
2016-07-25 19:45:39 -0400

    Create German.js
    - Added German translation by Bigtuber (http://fmdesign.forumotion.com/t729-forumactif-edge-german-translation#14318)

---
+++ lang/German.js
@@ -0,0 +1,134 @@
+// English Translation
+FAE.lang = {
+
+  language : 'Deutsch',
+
+  css : {
+    offline : 'OFFLINE',
+    online : 'ONLINE',
+    total_posts : 'Gesamt-Beiträge',
+    total_topics : 'Gesamt-Themen',
+    total_users : 'Gesamt-Mitglieder',
+    record_users : 'Die meisten Besucher, die jemals online waren',
+    board_age : 'Alter des Forums (in Tagen)',
+    newest_member : 'Neuestes Mitglied',
+    hidebox : 'Versteckt:',
+    chatbox_tip : 'Über diesen Bereich schweben, um zu sehen, wer online ist.',
+    chatbox_no_away : 'Keine Mitglieder sind zurzeit weg..',
+    chatbox_no_online : 'Keine Mitglieder sind zurzeit online.',
+  },
+
+
+  images : {
+
+    buttons : {
+      i_post : 'http://i35.servimg.com/u/f35/18/45/41/65/new-to10.png?color=primary&size=m',
+      i_reply : 'http://i35.servimg.com/u/f35/18/45/41/65/post-r11.png?color=primary&size=m',
+      i_reply_locked : 'http://i35.servimg.com/u/f35/18/45/41/65/locked12.png?color=secondary&size=m',
+      i_icon_quote : 'http://i35.servimg.com/u/f35/18/45/41/65/quote-11.png?color=primary&size=s',
+      i_icon_multiquote_off : 'http://i35.servimg.com/u/f35/18/45/41/65/multi-11.png?color=primary&size=s',
+      i_icon_multiquote_on : 'http://i35.servimg.com/u/f35/18/45/41/65/multi-11.png?color=secondary&size=s',
+      i_icon_thanks_off : 'http://i35.servimg.com/u/f35/18/45/41/65/thanks11.png?color=primary&size=s',
+      i_icon_thanks_on : 'http://i35.servimg.com/u/f35/18/45/41/65/thanks11.png?color=secondary&size=s',
+      i_icon_edit : 'http://i35.servimg.com/u/f35/18/45/41/65/edit-d10.png?color=primary&size=s',
+      i_icon_delete : 'http://i86.servimg.com/u/f86/18/21/41/30/delete10.png?color=primary&size=s',
+      i_icon_ip : 'http://i86.servimg.com/u/f86/18/21/41/30/ip-f10.png?color=primary&size=s',
+      i_icon_report : 'http://i86.servimg.com/u/f86/18/21/41/30/report10.png?color=primary&size=s',
+      i_icon_report_new : 'http://i86.servimg.com/u/f86/18/21/41/30/report10.png?color=secondary&size=s',
+      i_icon_report_locked : 'http://i86.servimg.com/u/f86/18/21/41/30/report12.png?color=secondary&size=s',
+      i_icon_lock_report : 'http://i86.servimg.com/u/f86/18/21/41/30/report12.png?color=primary&size=s',
+      i_icon_unlock_report : 'http://i86.servimg.com/u/f86/18/21/41/30/report13.png?color=primary&size=s',
+      i_icon_search : FAE.eGIF,
+      i_icon_profile : 'http://i86.servimg.com/u/f86/18/21/41/30/profil12.png?color=primary&size=s',
+      i_icon_www : 'http://i86.servimg.com/u/f86/18/21/41/30/websit10.png?color=primary&size=s',
+      i_icon_email : 'http://i86.servimg.com/u/f86/18/21/41/30/email-10.png?color=primary&size=s',
+      i_icon_pm : 'http://i86.servimg.com/u/f86/18/21/41/30/pm-f11.png?color=primary&size=s',
+      i_icon_fb : 'http://i86.servimg.com/u/f86/18/21/41/30/facebo11.png?color=primary&size=s',
+      i_icon_twitter : 'http://i86.servimg.com/u/f86/18/21/41/30/twitte10.png?color=primary&size=s',
+      i_icon_pinterest : 'http://i86.servimg.com/u/f86/18/21/41/30/pinter10.png?color=primary&size=s',
+      i_icon_aim : 'http://i86.servimg.com/u/f86/18/21/41/30/aim-f10.png?color=primary&size=s',
+      i_icon_icq_add : 'http://i86.servimg.com/u/f86/18/21/41/30/icq-f10.png?color=primary&size=s',
+      i_icon_msnm : 'http://i86.servimg.com/u/f86/18/21/41/30/msn-f10.png?color=primary&size=s',
+      i_icon_yim : 'http://i86.servimg.com/u/f86/18/21/41/30/yahoo-10.png?color=primary&size=s',
+      i_icon_skype : 'http://i86.servimg.com/u/f86/18/21/41/30/skype-10.png?color=primary&size=s',
+      i_icon_online : FAE.eGIF,
+      i_msg_newpost : 'http://i35.servimg.com/u/f35/18/45/41/65/new-pm11.png?color=primary&size=m',
+      i_msg_inbox : FAE.eGIF,
+      i_msg_sentbox : FAE.eGIF,
+      i_msg_outbox : FAE.eGIF,
+      i_msg_savebox : FAE.eGIF,
+      i_icon_ajax_edit : 'http://i86.servimg.com/u/f86/18/21/41/30/edit-f10.png',
+      i_icon_ajax_valid : 'http://i86.servimg.com/u/f86/18/21/41/30/valid-10.png',
+      i_icon_attachment_see : 'http://i86.servimg.com/u/f86/18/21/41/30/show-a10.png',
+      i_icon_attachment_download : 'http://i86.servimg.com/u/f86/18/21/41/30/downlo10.png',
+
+      // DO NOT TRANSLATE
+      page : 'buttons',
+      submit : 'Save'
+    },
+
+    gallery : {
+      upload_pic : 'http://i86.servimg.com/u/f86/18/21/41/30/upload10.png?color=primary&size=m'
+    }
+
+  },
+
+
+  javascripts : {
+
+    '[FA EDGE] ALL.JS' : {
+      widget_menu_title : 'Modul-Menü',
+      widget_menu_tooltip : 'Modul-Menü umschalten',
+
+      select_theme : 'Ein Thema auswählen',
+      random_theme : 'Zufälliges Thema',
+      default_theme : 'Voreingestelltes Thema',
+
+      toolbar_search_placeholder : 'Suchen...',
+
+      quick_nav_title : 'Schnelle Navigation',
+      quick_nav_tooltip : 'Schnelle Navigation umschalten',
+      quick_nav_notif : 'Benachrichtigungen',
+      tool_copy_url_tooltip : 'BBCode URL kopieren',
+      tool_copy_url_prompt : 'Kopieren Sie die unten stehende BBCode URL. (CTRL+C)',
+      tool_admin_panel : 'Administrations-Bereich',
+      tool_to_top : 'Anfang der Seite',
+      tool_to_bottom : 'Ende der Seite',
+
+      img_resizer_full_size : 'Volle Größe anzeigen',
+      img_resizer_enlarge : 'Bild vergrößern',
+      img_resizer_reduce : 'Bild verkleinern',
+      img_resizer_download : 'Bild downloaden',
+      img_resizer_tooltip : 'Klicken, um gesamtes Bild anzuschauen',
+
+      fae_update_found : 'Es wurde ein Update für Forumactif Edge gefunden. Wenn Sie bereit sind, gehen Sie bitte zu Ihrem FAE Bedienungsfeld und klicken Sie auf "Check for updates", um den Update-Prozess zu starten.'
+    }
+
+  },
+
+
+  templates : {
+
+    'overall_footer_end.html' : {
+      back_to_top : 'Zurück zum Anfang',
+      fae_feedback : 'Feedback einsenden',
+      fae_feedback_tooltip : 'Helfen, Forumactif Edge zu verbessern'
+    },
+
+
+    logged_out_reply : {
+      main_title : '<h2 class="lor_maintitle">Erstelle Sie einen Account oder loggen Sie sich ein, um zu antworten</h2>',
+      main_desc : '<p class="lor_maindesc">Sie müssen ein Mitglied sein, um antworten zu können.</p>',
+
+      create_account_title : '<h3 class="lor_subtitle">Einen Account erstellen</h3>',
+      create_account_desc : '<p class="lor_subdesc">Treten Sie unserer Community bei, indem Sie einen Account erstellen. Es ist einfach!</p>',
+      create_account_button : '<a href="/register" class="button1">Einen neuen Account erstellen</a>',
+
+      log_in_title : '<h3 class="lor_subtitle">Login</h3>',
+      log_in_desc : '<p class="lor_subdesc">Sie haben bereits einen Account? Kein Problem, hier können Sie sich einloggen.</p>',
+      log_in_button : '<a href="/login" class="button1">Login</a>'
+    }
+
+  }
+
+};

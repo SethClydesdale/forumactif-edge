428483be0f9e41179883d11eac224f43f950fd9c
Seth Clydesdale <sethc1995@gmail.com>
2016-07-26 07:07:43 -0400

    Create Dutch.js
    - Added Dutch (NL) translation by Samantha (http://fmdesign.forumotion.com/t738-forumactif-edge-dutch-translation#14469)

---
+++ lang/Dutch.js
@@ -0,0 +1,135 @@
+// Dutch (NL) Translation
+// Translation by Samantha (http://fmdesign.forumotion.com/t738-forumactif-edge-dutch-translation#14469)
+FAE.lang = {
+
+  language : 'Dutch',
+
+  css : {
+    offline : 'OFFLINE',
+    online : 'ONLINE',
+    total_posts : 'Totaal aantal berichten',
+    total_topics : 'Totaal aantal topics',
+    total_users : 'Totaal aantal leden',
+    record_users : 'Meeste leden ooit online',
+    board_age : 'Leeftijd van het forum (in dagen)',
+    newest_member : 'Nieuwste lid',
+    hidebox : 'Verborgen:',
+    chatbox_tip : 'Beweeg hier met de muis over om te zien wie er online is.',
+    chatbox_no_away : 'Momenteel zijn er geen leden inactief.',
+    chatbox_no_online : 'Er zijn geen leden online.',
+  },
+
+
+  images : {
+
+    buttons : {
+      i_post : 'http://i35.servimg.com/u/f35/18/21/60/73/new-to11.png?color=primary&size=m',
+      i_reply : 'http://i35.servimg.com/u/f35/18/21/60/73/post-r11.png?color=primary&size=m',
+      i_reply_locked : 'http://i35.servimg.com/u/f35/18/21/60/73/locked11.png?color=secondary&size=m',
+      i_icon_quote : 'http://i35.servimg.com/u/f35/18/21/60/73/quote-11.png?color=primary&size=s',
+      i_icon_multiquote_off : 'http://i35.servimg.com/u/f35/18/21/60/73/multi-11.png?color=primary&size=s',
+      i_icon_multiquote_on : 'http://i35.servimg.com/u/f35/18/21/60/73/multi-11.png?color=secondary&size=s',
+      i_icon_thanks_off : 'http://i35.servimg.com/u/f35/18/21/60/73/thanks11.png?color=primary&size=s',
+      i_icon_thanks_on : 'http://i35.servimg.com/u/f35/18/21/60/73/thanks11.png?color=secondary&size=s',
+      i_icon_edit : 'http://i35.servimg.com/u/f35/18/21/60/73/edit-n10.png?color=primary&size=s',
+      i_icon_delete : 'http://i86.servimg.com/u/f86/18/21/41/30/delete10.png?color=primary&size=s',
+      i_icon_ip : 'http://i86.servimg.com/u/f86/18/21/41/30/ip-f10.png?color=primary&size=s',
+      i_icon_report : 'http://i86.servimg.com/u/f86/18/21/41/30/report10.png?color=primary&size=s',
+      i_icon_report_new : 'http://i86.servimg.com/u/f86/18/21/41/30/report10.png?color=secondary&size=s',
+      i_icon_report_locked : 'http://i86.servimg.com/u/f86/18/21/41/30/report12.png?color=secondary&size=s',
+      i_icon_lock_report : 'http://i86.servimg.com/u/f86/18/21/41/30/report12.png?color=primary&size=s',
+      i_icon_unlock_report : 'http://i86.servimg.com/u/f86/18/21/41/30/report13.png?color=primary&size=s',
+      i_icon_search : FAE.eGIF,
+      i_icon_profile : 'http://i86.servimg.com/u/f86/18/21/41/30/profil12.png?color=primary&size=s',
+      i_icon_www : 'http://i86.servimg.com/u/f86/18/21/41/30/websit10.png?color=primary&size=s',
+      i_icon_email : 'http://i86.servimg.com/u/f86/18/21/41/30/email-10.png?color=primary&size=s',
+      i_icon_pm : 'http://i86.servimg.com/u/f86/18/21/41/30/pm-f11.png?color=primary&size=s',
+      i_icon_fb : 'http://i86.servimg.com/u/f86/18/21/41/30/facebo11.png?color=primary&size=s',
+      i_icon_twitter : 'http://i86.servimg.com/u/f86/18/21/41/30/twitte10.png?color=primary&size=s',
+      i_icon_pinterest : 'http://i86.servimg.com/u/f86/18/21/41/30/pinter10.png?color=primary&size=s',
+      i_icon_aim : 'http://i86.servimg.com/u/f86/18/21/41/30/aim-f10.png?color=primary&size=s',
+      i_icon_icq_add : 'http://i86.servimg.com/u/f86/18/21/41/30/icq-f10.png?color=primary&size=s',
+      i_icon_msnm : 'http://i86.servimg.com/u/f86/18/21/41/30/msn-f10.png?color=primary&size=s',
+      i_icon_yim : 'http://i86.servimg.com/u/f86/18/21/41/30/yahoo-10.png?color=primary&size=s',
+      i_icon_skype : 'http://i86.servimg.com/u/f86/18/21/41/30/skype-10.png?color=primary&size=s',
+      i_icon_online : FAE.eGIF,
+      i_msg_newpost : 'http://i35.servimg.com/u/f35/18/21/60/73/new-pm11.png?color=primary&size=m',
+      i_msg_inbox : FAE.eGIF,
+      i_msg_sentbox : FAE.eGIF,
+      i_msg_outbox : FAE.eGIF,
+      i_msg_savebox : FAE.eGIF,
+      i_icon_ajax_edit : 'http://i86.servimg.com/u/f86/18/21/41/30/edit-f10.png',
+      i_icon_ajax_valid : 'http://i86.servimg.com/u/f86/18/21/41/30/valid-10.png',
+      i_icon_attachment_see : 'http://i86.servimg.com/u/f86/18/21/41/30/show-a10.png',
+      i_icon_attachment_download : 'http://i86.servimg.com/u/f86/18/21/41/30/downlo10.png',
+
+      // DO NOT TRANSLATE
+      page : 'buttons',
+      submit : 'Save'
+    },
+
+    gallery : {
+      upload_pic : 'http://i35.servimg.com/u/f35/18/21/60/73/upload10.png?color=primary&size=m'
+    }
+
+  },
+
+
+  javascripts : {
+
+    '[FA EDGE] ALL.JS' : {
+      widget_menu_title : '<div class="title module_column_title">Widget Menu</div>',
+      widget_menu_tooltip : "menu.title = 'Widget menu laten zien of verbergen';",
+
+      select_theme : 'Kies een thema',
+      random_theme : 'Willekeurig thema',
+      default_theme : 'Standaard thema',
+
+      toolbar_search_placeholder : 'Zoeken...',
+
+      quick_nav_title : "title : 'Snelle Navigatie'",
+      quick_nav_tooltip : "tooltip : 'Snelle Navigatie (de)activeren'",
+      quick_nav_notif : 'Notificaties',
+      tool_copy_url_tooltip : 'Kopieer BBCode URL',
+      tool_copy_url_prompt : 'Kopieer BBCode URL hierbeneden. (CTRL+C)',
+      tool_admin_panel : 'Beheerderspaneel',
+      tool_to_top : 'Top van de pagina',
+      tool_to_bottom : 'Bodem van de pagina',
+
+      img_resizer_full_size : 'Op ware grootte laten zien',
+      img_resizer_enlarge : 'Vergroot plaatje',
+      img_resizer_reduce : 'Verklein plaatje',
+      img_resizer_download : 'Download plaatje',
+      img_resizer_tooltip : 'Klik om het volledige plaatje te laten zien',
+
+      fae_update_found : 'Er is een update voor Forumactif Edge gevonden. Om het installatieproces te beginnen, klik op "Updates Zoeken" vanuit het FAE Controllepaneel.'
+    }
+
+  },
+
+
+  templates : {
+
+    'overall_footer_end.html' : {
+      back_to_top : 'Terug naar boven',
+      fae_feedback : 'Feedback versturen',
+      fae_feedback_tooltip : 'Help Forumactif Edge te verbeteren'
+    },
+
+
+    logged_out_reply : {
+      main_title : '<h2 class="lor_maintitle">Registreer of meld je aan om te kunnen reageren</h2>',
+      main_desc : '<p class="lor_maindesc">Je moet lid zijn om te kunnen reageren.</p>',
+
+      create_account_title : '<h3 class="lor_subtitle">Registreer</h3>',
+      create_account_desc : '<p class="lor_subdesc">Word lid van onze community door te registreren. Het is makkelijk!</p>',
+      create_account_button : '<a href="/register" class="button1">Maak een nieuw account</a>',
+
+      log_in_title : '<h3 class="lor_subtitle">Aanmelden</h3>',
+      log_in_desc : '<p class="lor_subdesc">Heb je al een account? Geen probleem! Meld je hier aan.</p>',
+      log_in_button : '<a href="/login" class="button1">Aanmelden</a>'
+    }
+
+  }
+
+};

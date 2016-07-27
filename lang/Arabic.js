17c062642d9b34c44eb58a7a6e0ba19f46a70037
Seth Clydesdale <sethc1995@gmail.com>
2016-07-27 12:52:09 -0400

    Create Arabic.js
    - Added Arabic translation by omarpop23 (http://fmdesign.forumotion.com/t740-forumactif-edge-arabic-translation#14583)

---
+++ lang/Arabic.js
@@ -0,0 +1,135 @@
+// Arabic Translation
+// Translation by omarpop23 (http://fmdesign.forumotion.com/t740-forumactif-edge-arabic-translation#14583)
+FAE.lang = {
+
+  language : 'العربية',
+
+  css : {
+    offline : 'غير مُتصل',
+    online : 'مُتصل',
+    total_posts : 'اجمالي المساهمات',
+    total_topics : 'اجمالي المواضيع',
+    total_users : 'اجمالي الأعضاء',
+    record_users : 'أكثر الأعضاء تواجداً',
+    board_age : 'عُمر المنتدى (بلأيام)',
+    newest_member : 'آخر عُضو مُسجل',
+    hidebox : 'مخفي:',
+    chatbox_tip : 'مرر اللفأرة لترى المتصل و الغائب.',
+    chatbox_no_away : 'غائب.',
+    chatbox_no_online : 'مُتصل.',
+  },
+
+
+  images : {
+
+    buttons : {
+      i_post : 'http://i35.servimg.com/u/f35/18/21/60/73/new-to12.png?color=primary&size=m',
+      i_reply : 'http://i35.servimg.com/u/f35/18/21/60/73/post-r12.png?color=primary&size=m',
+      i_reply_locked : 'http://i35.servimg.com/u/f35/18/21/60/73/locked12.png?color=secondary&size=m',
+      i_icon_quote : 'http://i35.servimg.com/u/f35/18/21/60/73/quote-12.png?color=primary&size=s',
+      i_icon_multiquote_off : 'http://i35.servimg.com/u/f35/18/21/60/73/multi-12.png?color=primary&size=s',
+      i_icon_multiquote_on : 'http://i35.servimg.com/u/f35/18/21/60/73/multi-12.png?color=secondary&size=s',
+      i_icon_thanks_off : 'http://i35.servimg.com/u/f35/18/21/60/73/thanks12.png?color=primary&size=s',
+      i_icon_thanks_on : 'http://i35.servimg.com/u/f35/18/21/60/73/thanks12.png?color=secondary&size=s',
+      i_icon_edit : 'http://i35.servimg.com/u/f35/18/21/60/73/edit-a10.png?color=primary&size=s',
+      i_icon_delete : 'http://i86.servimg.com/u/f86/18/21/41/30/delete10.png?color=primary&size=s',
+      i_icon_ip : 'http://i86.servimg.com/u/f86/18/21/41/30/ip-f10.png?color=primary&size=s',
+      i_icon_report : 'http://i86.servimg.com/u/f86/18/21/41/30/report10.png?color=primary&size=s',
+      i_icon_report_new : 'http://i86.servimg.com/u/f86/18/21/41/30/report10.png?color=secondary&size=s',
+      i_icon_report_locked : 'http://i86.servimg.com/u/f86/18/21/41/30/report12.png?color=secondary&size=s',
+      i_icon_lock_report : 'http://i86.servimg.com/u/f86/18/21/41/30/report12.png?color=primary&size=s',
+      i_icon_unlock_report : 'http://i86.servimg.com/u/f86/18/21/41/30/report13.png?color=primary&size=s',
+      i_icon_search : FAE.eGIF,
+      i_icon_profile : 'http://i86.servimg.com/u/f86/18/21/41/30/profil12.png?color=primary&size=s',
+      i_icon_www : 'http://i86.servimg.com/u/f86/18/21/41/30/websit10.png?color=primary&size=s',
+      i_icon_email : 'http://i86.servimg.com/u/f86/18/21/41/30/email-10.png?color=primary&size=s',
+      i_icon_pm : 'http://i86.servimg.com/u/f86/18/21/41/30/pm-f11.png?color=primary&size=s',
+      i_icon_fb : 'http://i86.servimg.com/u/f86/18/21/41/30/facebo11.png?color=primary&size=s',
+      i_icon_twitter : 'http://i86.servimg.com/u/f86/18/21/41/30/twitte10.png?color=primary&size=s',
+      i_icon_pinterest : 'http://i86.servimg.com/u/f86/18/21/41/30/pinter10.png?color=primary&size=s',
+      i_icon_aim : 'http://i86.servimg.com/u/f86/18/21/41/30/aim-f10.png?color=primary&size=s',
+      i_icon_icq_add : 'http://i86.servimg.com/u/f86/18/21/41/30/icq-f10.png?color=primary&size=s',
+      i_icon_msnm : 'http://i86.servimg.com/u/f86/18/21/41/30/msn-f10.png?color=primary&size=s',
+      i_icon_yim : 'http://i86.servimg.com/u/f86/18/21/41/30/yahoo-10.png?color=primary&size=s',
+      i_icon_skype : 'http://i86.servimg.com/u/f86/18/21/41/30/skype-10.png?color=primary&size=s',
+      i_icon_online : FAE.eGIF,
+      i_msg_newpost : 'http://i35.servimg.com/u/f35/18/21/60/73/new-pm12.png?color=primary&size=m',
+      i_msg_inbox : FAE.eGIF,
+      i_msg_sentbox : FAE.eGIF,
+      i_msg_outbox : FAE.eGIF,
+      i_msg_savebox : FAE.eGIF,
+      i_icon_ajax_edit : 'http://i86.servimg.com/u/f86/18/21/41/30/edit-f10.png',
+      i_icon_ajax_valid : 'http://i86.servimg.com/u/f86/18/21/41/30/valid-10.png',
+      i_icon_attachment_see : 'http://i86.servimg.com/u/f86/18/21/41/30/show-a10.png',
+      i_icon_attachment_download : 'http://i86.servimg.com/u/f86/18/21/41/30/downlo10.png',
+
+      // DO NOT TRANSLATE
+      page : 'buttons',
+      submit : 'Save'
+    },
+
+    gallery : {
+      upload_pic : 'http://i35.servimg.com/u/f35/18/21/60/73/upload11.png?color=primary&size=m'
+    }
+
+  },
+
+
+  javascripts : {
+
+    '[FA EDGE] ALL.JS' : {
+      widget_menu_title : '<div class="title module_column_title">قائمة جانبية</div>',
+      widget_menu_tooltip : "menu.title = 'قائمة جانبية';",
+
+      select_theme : 'لونك المُفضل',
+      random_theme : 'لون عشوائي',
+      default_theme : 'اللون الأفتراضي',
+
+      toolbar_search_placeholder : 'بحــث...',
+
+      quick_nav_title : "title : 'الأنتقال السريع'",
+      quick_nav_tooltip : "tooltip : 'قائمة الأنتقال السريع'",
+      quick_nav_notif : 'الأشعارات',
+      tool_copy_url_tooltip : 'نسخ عنوان BBCode الـ للصفحة',
+      tool_copy_url_prompt : 'أنسخ عنوان الـ BBCode. (CTRL+C)',
+      tool_admin_panel : 'لوحة الأدارة',
+      tool_to_top : 'أعلي المنتدى',
+      tool_to_bottom : 'أسفل المنتدى',
+
+      img_resizer_full_size : 'عرض الحجم بالكامل',
+      img_resizer_enlarge : 'تصغير الصورة',
+      img_resizer_reduce : 'تكبير الصورة',
+      img_resizer_download : 'تحميل الصورة',
+      img_resizer_tooltip : 'أضغط هنا للعرض الطبيعي للصورة',
+
+      fae_update_found : 'تهانينا يتوفر تحديث جديد من نسخة المنتدي .'
+    }
+
+  },
+
+
+  templates : {
+
+    'overall_footer_end.html' : {
+      back_to_top : 'ألي أعلي',
+      fae_feedback : 'التغذية العكسية',
+      fae_feedback_tooltip : 'ساعدنا في تطوير نسخة المنتدى'
+    },
+
+
+    logged_out_reply : {
+      main_title : '<h2 class="lor_maintitle">انشئ حساب جديد او سجل دخولك لتترك رد</h2>',
+      main_desc : '<p class="lor_maindesc">لابد يكون لديك عضوية لتستطيع الرد سجل.</p>',
+
+      create_account_title : '<h3 class="lor_subtitle">التسجيل</h3>',
+      create_account_desc : '<p class="lor_subdesc">انضم الينا عالمنا الآنز فعملية التسجيل سهله جدا !</p>',
+      create_account_button : '<a href="/register" class="button1">أنشئ حساب جديد</a>',
+
+      log_in_title : '<h3 class="lor_subtitle">الدخول</h3>',
+      log_in_desc : '<p class="lor_subdesc">لديك عضوية؟ لا يوجد مشكلة سجل من هنا.</p>',
+      log_in_button : '<a href="/login" class="button1">الدخول</a>'
+    }
+
+  }
+
+};

1a3c965bec650b9cb6757d618fa3e8b6d9701735
Seth Clydesdale <sethc1995@gmail.com>
2016-07-29 11:10:43 -0400

    Create Romanian.js
    - Added Romanian translation by SSYT (http://fmdesign.forumotion.com/t751-forumactif-edge-romanian-translation-partial#14953)

---
+++ lang/Romanian.js
@@ -0,0 +1,135 @@
+// Romanian Translation
+// Translation by SSYT (http://fmdesign.forumotion.com/t751-forumactif-edge-romanian-translation-partial#14953)
+FAE.lang = {
+
+  language : 'Romana',
+
+  css : {
+    offline : 'DECONECTAT',
+    online : 'CONECTAT',
+    total_posts : 'Total Mesaje',
+    total_topics : 'Total Postari',
+    total_users : 'Total Membri',
+    record_users : 'Cei mai multi conectati',
+    board_age : 'Varsta forum (zile)',
+    newest_member : 'Ultimul inregistrat',
+    hidebox : 'Ascuns:',
+    chatbox_tip : 'Hover over to see who is online.',
+    chatbox_no_away : 'Nu sunt membri afk monentan..',
+    chatbox_no_online : 'Nu sunt membri online momentan.',
+  },
+
+
+  images : {
+
+    buttons : {
+      i_post : 'http://i35.servimg.com/u/f35/18/21/60/73/new-to13.png?color=primary&size=m',
+      i_reply : 'http://i35.servimg.com/u/f35/18/21/60/73/post-r13.png?color=primary&size=m',
+      i_reply_locked : 'http://i35.servimg.com/u/f35/18/21/60/73/locked13.png?color=secondary&size=m',
+      i_icon_quote : 'http://i35.servimg.com/u/f35/18/21/60/73/quote-13.png?color=primary&size=s',
+      i_icon_multiquote_off : 'http://i35.servimg.com/u/f35/18/21/60/73/multi-13.png?color=primary&size=s',
+      i_icon_multiquote_on : 'http://i35.servimg.com/u/f35/18/21/60/73/multi-13.png?color=secondary&size=s',
+      i_icon_thanks_off : 'http://i35.servimg.com/u/f35/18/21/60/73/thanks13.png?color=primary&size=s',
+      i_icon_thanks_on : 'http://i35.servimg.com/u/f35/18/21/60/73/thanks13.png?color=secondary&size=s',
+      i_icon_edit : 'http://i35.servimg.com/u/f35/18/21/60/73/edit-r10.png?color=primary&size=s',
+      i_icon_delete : 'http://i86.servimg.com/u/f86/18/21/41/30/delete10.png?color=primary&size=s',
+      i_icon_ip : 'http://i86.servimg.com/u/f86/18/21/41/30/ip-f10.png?color=primary&size=s',
+      i_icon_report : 'http://i86.servimg.com/u/f86/18/21/41/30/report10.png?color=primary&size=s',
+      i_icon_report_new : 'http://i86.servimg.com/u/f86/18/21/41/30/report10.png?color=secondary&size=s',
+      i_icon_report_locked : 'http://i86.servimg.com/u/f86/18/21/41/30/report12.png?color=secondary&size=s',
+      i_icon_lock_report : 'http://i86.servimg.com/u/f86/18/21/41/30/report12.png?color=primary&size=s',
+      i_icon_unlock_report : 'http://i86.servimg.com/u/f86/18/21/41/30/report13.png?color=primary&size=s',
+      i_icon_search : FAE.eGIF,
+      i_icon_profile : 'http://i86.servimg.com/u/f86/18/21/41/30/profil12.png?color=primary&size=s',
+      i_icon_www : 'http://i86.servimg.com/u/f86/18/21/41/30/websit10.png?color=primary&size=s',
+      i_icon_email : 'http://i86.servimg.com/u/f86/18/21/41/30/email-10.png?color=primary&size=s',
+      i_icon_pm : 'http://i86.servimg.com/u/f86/18/21/41/30/pm-f11.png?color=primary&size=s',
+      i_icon_fb : 'http://i86.servimg.com/u/f86/18/21/41/30/facebo11.png?color=primary&size=s',
+      i_icon_twitter : 'http://i86.servimg.com/u/f86/18/21/41/30/twitte10.png?color=primary&size=s',
+      i_icon_pinterest : 'http://i86.servimg.com/u/f86/18/21/41/30/pinter10.png?color=primary&size=s',
+      i_icon_aim : 'http://i86.servimg.com/u/f86/18/21/41/30/aim-f10.png?color=primary&size=s',
+      i_icon_icq_add : 'http://i86.servimg.com/u/f86/18/21/41/30/icq-f10.png?color=primary&size=s',
+      i_icon_msnm : 'http://i86.servimg.com/u/f86/18/21/41/30/msn-f10.png?color=primary&size=s',
+      i_icon_yim : 'http://i86.servimg.com/u/f86/18/21/41/30/yahoo-10.png?color=primary&size=s',
+      i_icon_skype : 'http://i86.servimg.com/u/f86/18/21/41/30/skype-10.png?color=primary&size=s',
+      i_icon_online : FAE.eGIF,
+      i_msg_newpost : 'http://i35.servimg.com/u/f35/18/21/60/73/new-pm13.png?color=primary&size=m',
+      i_msg_inbox : FAE.eGIF,
+      i_msg_sentbox : FAE.eGIF,
+      i_msg_outbox : FAE.eGIF,
+      i_msg_savebox : FAE.eGIF,
+      i_icon_ajax_edit : 'http://i86.servimg.com/u/f86/18/21/41/30/edit-f10.png',
+      i_icon_ajax_valid : 'http://i86.servimg.com/u/f86/18/21/41/30/valid-10.png',
+      i_icon_attachment_see : 'http://i86.servimg.com/u/f86/18/21/41/30/show-a10.png',
+      i_icon_attachment_download : 'http://i86.servimg.com/u/f86/18/21/41/30/downlo10.png',
+
+      // DO NOT TRANSLATE
+      page : 'buttons',
+      submit : 'Save'
+    },
+
+    gallery : {
+      upload_pic : 'http://i35.servimg.com/u/f35/18/21/60/73/upload12.png?color=primary&size=m'
+    }
+
+  },
+
+
+  javascripts : {
+
+    '[FA EDGE] ALL.JS' : {
+      widget_menu_title : '<div class="title module_column_title">Meniu Widget</div>',
+      widget_menu_tooltip : "menu.title = 'Toggle widget menu';",
+
+      select_theme : 'Selecteaza o tema',
+      random_theme : 'Tema random',
+      default_theme : 'Tema Implicita',
+
+      toolbar_search_placeholder : 'Cauta...',
+
+      quick_nav_title : "title : 'Quick Navigation'",
+      quick_nav_tooltip : "tooltip : 'Toggle quick navigation'",
+      quick_nav_notif : 'Notificari',
+      tool_copy_url_tooltip : 'Copy BBCode URL',
+      tool_copy_url_prompt : 'Copy the BBCode URL below. (CTRL+C)',
+      tool_admin_panel : 'Panou de administrare',
+      tool_to_top : 'Top of page',
+      tool_to_bottom : 'Bottom of page',
+
+      img_resizer_full_size : 'Show full size',
+      img_resizer_enlarge : 'Enlarge image',
+      img_resizer_reduce : 'Reduce image',
+      img_resizer_download : 'Download image',
+      img_resizer_tooltip : 'Click to view full image',
+
+      fae_update_found : 'An update has been found for Forumactif Edge. When you\'re ready, please proceed to your FAE Control Panel and click "Check for updates" to begin the update process.'
+    }
+
+  },
+
+
+  templates : {
+
+    'overall_footer_end.html' : {
+      back_to_top : 'Sus',
+      fae_feedback : 'Raporteaza un bug',
+      fae_feedback_tooltip : 'Ajuta la dezvoltarea temeio Forumactif Edge'
+    },
+
+
+    logged_out_reply : {
+      main_title : '<h2 class="lor_maintitle">Trebuie sa fii inregistrat pentru a posta</h2>',
+      main_desc : '<p class="lor_maindesc">Trebuie sa fii conectat pentru a posta.</p>',
+
+      create_account_title : '<h3 class="lor_subtitle">Creare cont</h3>',
+      create_account_desc : '<p class="lor_subdesc">Join our community by creating a new account. It\'s easy!</p>',
+      create_account_button : '<a href="/register" class="button1">Inregistrare</a>',
+
+      log_in_title : '<h3 class="lor_subtitle">Conectare</h3>',
+      log_in_desc : '<p class="lor_subdesc">Already have an account? No problem, log in here.</p>',
+      log_in_button : '<a href="/login" class="button1">Conectare</a>'
+    }
+
+  }
+
+};

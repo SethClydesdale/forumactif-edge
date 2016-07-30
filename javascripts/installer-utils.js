40b70ab64e90b3a70dd9fee4cd7209d5654c36bb
Seth Clydesdale <sethc1995@gmail.com>
2016-07-30 10:46:33 -0400

    Update installer-utils.js

--- javascripts/installer-utils.js
+++ javascripts/installer-utils.js
@@ -246,35 +246,33 @@
         // create and insert the theme switcher
         if (window.location.host == 'themedesign.forumotion.com') {
 
-          $(opts).append(
-            $('<br class="clear" />'+
-              '<div class="fae_cp_title">Theme Management</div>'+
-              '<p>This section allows you to import different themes for Forumactif Edge.</p>'+
+          $(opts).append('<br class="clear" />'+
+            '<div class="fae_cp_title">Theme Management</div>'+
+            '<p>This section allows you to import different themes for Forumactif Edge.</p>'+
 
-              '<div class="fae_cp_row">'+
-                '<span id="fae_label_theme" class="fae_label">Select a theme : </span>'+
-                '<select id="fae_selected_theme">'+
-                  '<option value="fa_edge">Edge Default</option>'+
-                  '<option value="fa_edge_dark">Edge Dark</option>'+
-                '</select>'+
-              '</div>'+
+            '<div class="fae_cp_row">'+
+              '<span id="fae_label_theme" class="fae_label">Select a theme : </span>'+
+              '<select id="fae_selected_theme">'+
+                '<option value="fa_edge">Edge Default</option>'+
+                '<option value="fa_edge_dark">Edge Dark</option>'+
+              '</select>'+
+            '</div>'+
 
-              '<div class="fae_cp_row">'+
-                '<span id="fae_label_min" class="fae_label">Minify Stylesheet : </span>'+
-                '<input id="fae_theme_min" type="checkbox" checked />'+
-                '<p id="fae_theme_min_desc">Leave this checked if you\'d like the stylesheet to be minified. Minified stylesheets take up less space, but aren\'t easily editable. Uncheck this box if you want to edit the stylesheet of the selected theme.</p>'+
-              '</div>'+
+            '<div class="fae_cp_row">'+
+              '<span id="fae_label_min" class="fae_label">Minify Stylesheet : </span>'+
+              '<input id="fae_theme_min" type="checkbox" checked />'+
+              '<p id="fae_theme_min_desc">Leave this checked if you\'d like the stylesheet to be minified. Minified stylesheets take up less space, but aren\'t easily editable. Uncheck this box if you want to edit the stylesheet of the selected theme.</p>'+
+            '</div>'+
 
-              '<div class="fae_cp_row">'+
-                '<span id="fae_label_rtl" class="fae_label">Right-to-Left : </span>'+
-                '<input id="fae_theme_dir" type="checkbox" />'+
-                '<p id="fae_theme_dir_desc">Changes the theme\'s text direction from left-to-right (ltr) to right-to-left. (rtl)</p>'+
-              '</div>'+
+            '<div class="fae_cp_row">'+
+              '<span id="fae_label_rtl" class="fae_label">Right-to-Left : </span>'+
+              '<input id="fae_theme_dir" type="checkbox" />'+
+              '<p id="fae_theme_dir_desc">Changes the theme\'s text direction from left-to-right (ltr) to right-to-left. (rtl)</p>'+
+            '</div>'+
 
-              '<div class="fae_cp_row">'+
-                '<input id="fae_change_css" type="button" value="Save theme changes" />'+
-              '</div>')[0]
-          );
+            '<div class="fae_cp_row">'+
+              '<input id="fae_change_css" type="button" value="Save theme changes" />'+
+            '</div>');
 
         }
 

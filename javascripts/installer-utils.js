8ff0547c9c5a1445a18255d9502a1fb3f8286ffe
Seth Clydesdale <sethc1995@gmail.com>
2016-07-30 11:16:30 -0400

    Update installer-utils.js

--- javascripts/installer-utils.js
+++ javascripts/installer-utils.js
@@ -251,6 +251,9 @@
             '<p>This section allows you to import different themes for Forumactif Edge.</p>'+
 
             '<div class="fae_cp_row">'+
+              '<span class="fae_help_me">?'+
+                '<span class="fae_help_tip">Choose the type of theme you want to apply to Forumactif Edge via the drop down.</span>'+
+              '</span>'+
               '<span id="fae_label_theme" class="fae_label">Select a theme : </span>'+
               '<select id="fae_selected_theme">'+
                 '<option value="fa_edge">Edge Default</option>'+
@@ -305,3 +308,14 @@
     }
   });
 }());
+
+// inject extra cp stylesheet
+$('head').append(
+  '<style type="text/css">'+
+    '.fae_cp_row { margin:6px 0; }'+
+    '.fae_label { display:inline-block; width:200px; }'+
+    '.fae_help_me { color:#FFF; font-size:18px; background:#69C; border-radius:100%; text-align:center; vertical-align:middle; display:inline-block; height:24px; line-height:24px; width:24px; margin-right:3px; position:relative; cursor:help; }'+
+    '.fae_help_tip { color:#333; font-size:12px; line-height:15px; background:#EEE; border:1px solid #CCC; display:inline-block; width:300px; padding:3px; position:absolute; left:28px; visibility:hidden; z-index:1; }'+
+    '.fae_help_me:hover .fae_help_tip { visibility:visible; }'+
+  '</style>'
+);

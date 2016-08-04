820cb6800cffe6db9c15aec956b58ec8f977da9f
Seth Clydesdale <sethc1995@gmail.com>
2016-08-04 11:38:03 -0400

    Update installer-utils.js
    -rtl should be before .min

--- javascripts/installer-utils.js
+++ javascripts/installer-utils.js
@@ -335,7 +335,7 @@
           document.getElementById('fae_change_css').onclick = function() {
             var select = document.getElementById('fae_selected_theme'),
                 selected = select.options[select.selectedIndex],
-                stylesheet = selected.value + ( document.getElementById('fae_theme_min_yes').checked ? '.min' : '' ) + ( document.getElementById('fae_theme_dir_rtl').checked ? '-rtl' : '' ) + '.css';
+                stylesheet = selected.value + ( document.getElementById('fae_theme_dir_rtl').checked ? '-rtl' : '' ) + ( document.getElementById('fae_theme_min_yes').checked ? '.min' : '' ) + '.css';
 
             if (confirm( 'Are you sure you want to import the theme "' + selected.innerHTML + '" ?\\\n\\\nPlease make sure to back up your current stylesheet if you want to keep it, because it will be overwritten when this new theme is imported. Choose "Cancel" if you\'re not ready to import a new theme.'.replace(/\\/g, '') )) {
               FAE.theme = {

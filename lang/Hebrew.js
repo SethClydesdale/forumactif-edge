ebc2616b5e8f8790aa1d4f832ee37ab773f033e0
Seth Clydesdale <sethc1995@gmail.com>
2016-08-23 13:06:18 -0400

    Create Hebrew.js
    Added Hebrew translation by Valoish

---
+++ lang/Hebrew.js
@@ -0,0 +1,170 @@
+// Hebrew Translation
+// Translation by Valoish (http://fmdesign.forumotion.com/t915-forumactif-edge-hebrew-translation#18318)
+FAE.lang = {
+
+  language : 'עברית',
+
+  css : {
+    offline : 'לא מחובר',
+    online : 'מחובר',
+    total_posts : 'סה"כ הודעות',
+    total_topics : 'סה"כ נושאים',
+    total_users : 'סה"כ משתמשים',
+    record_users : 'רוב המשתמשים שהתחברו',
+    board_age : '(גיל הפורום (ימים',
+    newest_member : 'המשתמש החדש ביותר שנרשם',
+    hidebox : 'מוסתר:',
+    chatbox_tip : '.העבר את העכבר מעל כדי לראות מי מחובר',
+    chatbox_no_away : '..אין משתמשים שאינם נמצעים על יד המקלדת כרגע',
+    chatbox_no_online : '.אין משתמשים מחוברים כרגע'
+  },
+
+
+  images : {
+
+    buttons : {
+      i_post : 'http://i35.servimg.com/u/f35/18/21/41/30/new-to11.png?color=primary&size=m',
+      i_reply : 'http://i35.servimg.com/u/f35/18/21/41/30/post-r12.png?color=primary&size=m',
+      i_reply_locked : 'http://i35.servimg.com/u/f35/18/21/41/30/locked14.png?color=secondary&size=m',
+      i_icon_quote : 'http://i35.servimg.com/u/f35/18/21/41/30/quote-14.png?color=primary&size=s',
+      i_icon_multiquote_off : 'http://i35.servimg.com/u/f35/18/21/41/30/multi-14.png?color=primary&size=s',
+      i_icon_multiquote_on : 'http://i35.servimg.com/u/f35/18/21/41/30/multi-14.png?color=secondary&size=s',
+      i_icon_thanks_off : 'http://i35.servimg.com/u/f35/18/21/41/30/thanks14.png?color=primary&size=s',
+      i_icon_thanks_on : 'http://i35.servimg.com/u/f35/18/21/41/30/thanks14.png?color=secondary&size=s',
+      i_icon_edit : 'http://i35.servimg.com/u/f35/18/21/41/30/edit-h10.png?color=primary&size=s',
+      i_icon_delete : 'http://i86.servimg.com/u/f86/18/21/41/30/delete10.png?color=primary&size=s',
+      i_icon_ip : 'http://i86.servimg.com/u/f86/18/21/41/30/ip-f10.png?color=primary&size=s',
+      i_icon_report : 'http://i86.servimg.com/u/f86/18/21/41/30/report10.png?color=primary&size=s',
+      i_icon_report_new : 'http://i86.servimg.com/u/f86/18/21/41/30/report10.png?color=secondary&size=s',
+      i_icon_report_locked : 'http://i86.servimg.com/u/f86/18/21/41/30/report12.png?color=secondary&size=s',
+      i_icon_lock_report : 'http://i86.servimg.com/u/f86/18/21/41/30/report12.png?color=primary&size=s',
+      i_icon_unlock_report : 'http://i86.servimg.com/u/f86/18/21/41/30/report13.png?color=primary&size=s',
+      i_icon_search : FAE.eGIF,
+      i_icon_profile : 'http://i86.servimg.com/u/f86/18/21/41/30/profil12.png?color=primary&size=s',
+      i_icon_www : 'http://i86.servimg.com/u/f86/18/21/41/30/websit10.png?color=primary&size=s',
+      i_icon_email : 'http://i86.servimg.com/u/f86/18/21/41/30/email-10.png?color=primary&size=s',
+      i_icon_pm : 'http://i86.servimg.com/u/f86/18/21/41/30/pm-f11.png?color=primary&size=s',
+      i_icon_fb : 'http://i86.servimg.com/u/f86/18/21/41/30/facebo11.png?color=primary&size=s',
+      i_icon_twitter : 'http://i86.servimg.com/u/f86/18/21/41/30/twitte10.png?color=primary&size=s',
+      i_icon_pinterest : 'http://i86.servimg.com/u/f86/18/21/41/30/pinter10.png?color=primary&size=s',
+      i_icon_aim : 'http://i86.servimg.com/u/f86/18/21/41/30/aim-f10.png?color=primary&size=s',
+      i_icon_icq_add : 'http://i86.servimg.com/u/f86/18/21/41/30/icq-f10.png?color=primary&size=s',
+      i_icon_msnm : 'http://i86.servimg.com/u/f86/18/21/41/30/msn-f10.png?color=primary&size=s',
+      i_icon_yim : 'http://i86.servimg.com/u/f86/18/21/41/30/yahoo-10.png?color=primary&size=s',
+      i_icon_skype : 'http://i86.servimg.com/u/f86/18/21/41/30/skype-10.png?color=primary&size=s',
+      i_icon_online : FAE.eGIF,
+      i_msg_newpost : 'http://i35.servimg.com/u/f35/18/21/41/30/new-pm11.png?color=primary&size=m',
+      i_msg_inbox : FAE.eGIF,
+      i_msg_sentbox : FAE.eGIF,
+      i_msg_outbox : FAE.eGIF,
+      i_msg_savebox : FAE.eGIF,
+      i_icon_ajax_edit : 'http://i86.servimg.com/u/f86/18/21/41/30/edit-f10.png',
+      i_icon_ajax_valid : 'http://i86.servimg.com/u/f86/18/21/41/30/valid-10.png',
+      i_icon_attachment_see : 'http://i86.servimg.com/u/f86/18/21/41/30/show-a10.png',
+      i_icon_attachment_download : 'http://i86.servimg.com/u/f86/18/21/41/30/downlo10.png',
+
+      // DO NOT TRANSLATE
+      page : 'buttons',
+      submit : 'Save'
+    },
+
+    gallery : {
+      upload_pic : 'http://i35.servimg.com/u/f35/18/21/41/30/upload12.png?color=primary&size=m'
+    }
+
+  },
+
+
+  javascripts : {
+
+    '[FA EDGE] ALL.JS' : {
+      widget_menu_title : '<div class="title module_column_title">תפריט יישומון</div>',
+      widget_menu_tooltip : "menu.title = 'הפעל תפריט יישומון';",
+
+      select_theme : 'סכמת צבעים',
+      random_theme : 'ערכת צבע אקראית',
+      default_theme : 'ערכת צבעי ברירת מחדל',
+
+      toolbar_search_placeholder : 'חיפוש...',
+
+      quick_nav_title : "title : 'ניווט מהיר'",
+      quick_nav_tooltip : "tooltip : 'הפעל ניווט מהיר'",
+      quick_nav_notif : 'נוטיפיקציות',
+      tool_copy_url_tooltip : 'העתק כתובת אתר BBCode',
+      tool_copy_url_prompt : 'העתק את כתובת האתר BBCode להלן. (Ctrl + C)',
+      tool_admin_panel : 'פאנל ניהול',
+      tool_to_top : 'ראש הדף',
+      tool_to_bottom : 'תחתית הדף',
+
+      img_resizer_full_size : 'הצג בגודל מלא',
+      img_resizer_enlarge : 'הגדל את התמונה',
+      img_resizer_reduce : 'הקטין את התמונה',
+      img_resizer_download : 'הורד את התמונה',
+      img_resizer_tooltip : 'לחץ להצגת התמונה המלאה',
+
+      fae_update_found : '.כאשר תהיה מוכן, בבקשה המשך אל לוח הבקרה שלך ולחץ על "בדוק עדכונים" כדי להתחיל את תהליך העדכון .Forumactif Edge עדכון כבר נמצא בשביל'
+    }
+
+  },
+
+
+  templates : {
+
+    'overall_footer_end.html' : {
+      back_to_top : 'חזרה לראש הדף',
+      fae_feedback : 'שלח משוב',
+      fae_feedback_tooltip : 'FAE עזור לשפר'
+    },
+
+
+    logged_out_reply : {
+      main_title : '<h2 class="lor_maintitle">צור חשבון או התחבר כדי לכתוב תגובה</h2>',
+      main_desc : '<p class="lor_maindesc">עליך להיות משתמש כדי להשאיר תגובה</p>',
+
+      create_account_title : '<h3 class="lor_subtitle">צור חשבון</h3>',
+      create_account_desc : '<p class="lor_subdesc">!הצטרף לקהילה שלנו על ידי יצירת חשבון חדש. זה קל</p>',
+      create_account_button : '<a href="/register" class="button1">צור חשבון</a>',
+
+      log_in_title : '<h3 class="lor_subtitle">התחבר</h3>',
+      log_in_desc : '<p class="lor_subdesc">כבר יש לך חשבון? אין בעיה, התחבר כאן.</p>',
+      log_in_button : '<a href="/login" class="button1">התחבר</a>'
+    }
+
+  },
+
+
+  fae_cp : {
+    fae_cp_main_title : 'לוח הבקרה של FAE',
+    fae_cp_desc : 'פאנל זה מאפשר לך להתקין, להסיר, ולעדכן <a href="https://github.com/SethClydesdale/forumactif-edge" target="_blank">Forumactif Edge</a>. התכונן לחוות את הפורום בצורה מודרנית ומגיבה יותר!',
+    fae_log : 'יומן',
+    fae_actions : 'אפשרויות',
+
+    fae_install : 'התקן',
+    fae_reinstall : 'התקן מחדש',
+    fae_install_complete : '!מושלם',
+    fae_install_warning : 'האם אתה בטוח שברצונך להתקין FAE? פעולה זו תגרום למחיקת העיצוב הנוכחי שלך ומחיקת הJS הנוכחי שלך.\\\n\\\n\\\n.לפני שתמשיך JS ,תבניות ,CSS הקפד לגבות את כל קבצי התוכן האישי שלך כמו\\\n.FAE לחץ על "ביטול" עם אינך מוכן להתקין',
+
+    fae_uninstall : 'הסר',
+    fae_uninstall_warning : 'האם אתה בטוח שברצונך להסיר FAE? פעולה זו תגרום למחיקת כל הJS, הCSS והשינויי התבנית הנוכחית שלך.\\\n\\\n\\\n.לפני שתמשיך JS ,תבניות ,CSS הקפד לגבות את כל קבצי התוכן האישי שלך כמו\\\n.עדייו FAE לחץ על "ביטול" עם אינך מוכן להסיר',
+
+    fae_update : 'בדוק אם יש עדכונים',
+    fae_update_good : '!מעודכן FAE',
+    fae_update_check : '...בודק אם יש עדכונים על גיט-הב',
+    fae_update_prepare : '...משיג הוראות עדכון, נא לא לסגור כרטיסייה זו',
+    fae_update_start : 'עדכונים עומדים להתחיל, נא לא לסגור כרטיסייה זו.',
+    fae_update_fetch : '{VERSION_STRING}... ({UPDATE_MIN}/{UPDATE_MAX}) משיג הוראות עדכון לגירסה ',
+    fae_update_notes : '.{VERSION_STRING} כדי להציג את השינויים שבוצעו בגירסה <a href="{RELEASE_TAG}" target="_blank"> לחץ כאן</a> אנא',
+    fae_update_error : 'לא נמצאו. אנא {VERSION_STRING} הוראות העדכון לגרסה.ותספק מידע זה לקבלת סיוע נוסף <a href="http://fmdesign.forumotion.com/t700-forumactif-edge-support#13923" target="_blank">פתח נושא חדש פה</a>',
+
+    fae_translate : 'שנה שפה',
+    fae_translate_same : '.בחר שפה אחרת .{LANG}כבר ב FAE',
+    fae_translate_warning : '?{LANG}ל FAE האם אתה בטוח שאתה רוצה לשנות את השפה של',
+    fae_translate_start : '.יחל בקרוב. המתן בבקשה FAE תרגום של',
+    fae_translate_get : '.{LANG}משיג נתוני שפה של',
+    fae_translate_loaded : '.נתוני שפה נטענו. תהליך התרגום כעת יתחיל, נא לא לסגור כרטיסייה זו',
+
+    fae_err_not_founder : '.FAE רשאי להשתמש בלוח בקרה זה. פנה אליו לקבלת סיוע עם התקנת <a href="/u1">המייסד</a> רק'
+
+  }
+
+};

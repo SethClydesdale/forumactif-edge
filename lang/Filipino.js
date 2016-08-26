e49d22bef9dd82a9790ed1f70ca4647b619434fd
Seth Clydesdale <sethc1995@gmail.com>
2016-08-26 12:21:19 -0400

    Create Filipino.js

---
+++ lang/Filipino.js
@@ -0,0 +1,170 @@
+// Filipino Translation
+// Translation by Wilson (http://fmdesign.forumotion.com/t990-forumactif-edge-filipino-translation#20084)
+FAE.lang = {
+
+  language : 'Tagalog',
+
+  css : {
+    offline : 'NAKA-OFFLINE',
+    online : 'NAKA-ONLINE',
+    total_posts : 'Total ng mga posts',
+    total_topics : 'Total ng mga topics',
+    total_users : 'Total ng mga miyembro',
+    record_users : 'Pinakamaraming miyembrong online',
+    board_age : 'Edad ng forum (araw)',
+    newest_member : 'Bagong miyembro',
+    hidebox : 'Nakakubli:',
+    chatbox_tip : 'I-hover dito para makita ang mga online',
+    chatbox_no_away : 'Walang miyembro ang kasalukuyang wala rito.',
+    chatbox_no_online : 'Walang miyembro ang kasalukuyang nandito.'
+  },
+
+
+  images : {
+
+    buttons : {
+      i_post : 'http://i35.servimg.com/u/f35/18/21/60/73/new-to15.png?color=primary&size=m',
+      i_reply : 'http://i35.servimg.com/u/f35/18/21/60/73/post-r15.png?color=primary&size=m',
+      i_reply_locked : 'http://i35.servimg.com/u/f35/18/21/60/73/locked15.png?color=secondary&size=m',
+      i_icon_quote : 'http://i35.servimg.com/u/f35/18/21/60/73/quote-15.png?color=primary&size=s',
+      i_icon_multiquote_off : 'http://i35.servimg.com/u/f35/18/21/60/73/multi-15.png?color=primary&size=s',
+      i_icon_multiquote_on : 'http://i35.servimg.com/u/f35/18/21/60/73/multi-15.png?color=secondary&size=s',
+      i_icon_thanks_off : 'http://i35.servimg.com/u/f35/18/21/60/73/thanks15.png?color=primary&size=s',
+      i_icon_thanks_on : 'http://i35.servimg.com/u/f35/18/21/60/73/thanks15.png?color=secondary&size=s',
+      i_icon_edit : 'http://i35.servimg.com/u/f35/18/21/60/73/edit-t10.png?color=primary&size=s',
+      i_icon_delete : 'http://i86.servimg.com/u/f86/18/21/41/30/delete10.png?color=primary&size=s',
+      i_icon_ip : 'http://i86.servimg.com/u/f86/18/21/41/30/ip-f10.png?color=primary&size=s',
+      i_icon_report : 'http://i86.servimg.com/u/f86/18/21/41/30/report10.png?color=primary&size=s',
+      i_icon_report_new : 'http://i86.servimg.com/u/f86/18/21/41/30/report10.png?color=secondary&size=s',
+      i_icon_report_locked : 'http://i86.servimg.com/u/f86/18/21/41/30/report12.png?color=secondary&size=s',
+      i_icon_lock_report : 'http://i86.servimg.com/u/f86/18/21/41/30/report12.png?color=primary&size=s',
+      i_icon_unlock_report : 'http://i86.servimg.com/u/f86/18/21/41/30/report13.png?color=primary&size=s',
+      i_icon_search : FAE.eGIF,
+      i_icon_profile : 'http://i86.servimg.com/u/f86/18/21/41/30/profil12.png?color=primary&size=s',
+      i_icon_www : 'http://i86.servimg.com/u/f86/18/21/41/30/websit10.png?color=primary&size=s',
+      i_icon_email : 'http://i86.servimg.com/u/f86/18/21/41/30/email-10.png?color=primary&size=s',
+      i_icon_pm : 'http://i86.servimg.com/u/f86/18/21/41/30/pm-f11.png?color=primary&size=s',
+      i_icon_fb : 'http://i86.servimg.com/u/f86/18/21/41/30/facebo11.png?color=primary&size=s',
+      i_icon_twitter : 'http://i86.servimg.com/u/f86/18/21/41/30/twitte10.png?color=primary&size=s',
+      i_icon_pinterest : 'http://i86.servimg.com/u/f86/18/21/41/30/pinter10.png?color=primary&size=s',
+      i_icon_aim : 'http://i86.servimg.com/u/f86/18/21/41/30/aim-f10.png?color=primary&size=s',
+      i_icon_icq_add : 'http://i86.servimg.com/u/f86/18/21/41/30/icq-f10.png?color=primary&size=s',
+      i_icon_msnm : 'http://i86.servimg.com/u/f86/18/21/41/30/msn-f10.png?color=primary&size=s',
+      i_icon_yim : 'http://i86.servimg.com/u/f86/18/21/41/30/yahoo-10.png?color=primary&size=s',
+      i_icon_skype : 'http://i86.servimg.com/u/f86/18/21/41/30/skype-10.png?color=primary&size=s',
+      i_icon_online : FAE.eGIF,
+      i_msg_newpost : 'http://i35.servimg.com/u/f35/18/21/60/73/new-pm15.png?color=primary&size=m',
+      i_msg_inbox : FAE.eGIF,
+      i_msg_sentbox : FAE.eGIF,
+      i_msg_outbox : FAE.eGIF,
+      i_msg_savebox : FAE.eGIF,
+      i_icon_ajax_edit : 'http://i86.servimg.com/u/f86/18/21/41/30/edit-f10.png',
+      i_icon_ajax_valid : 'http://i86.servimg.com/u/f86/18/21/41/30/valid-10.png',
+      i_icon_attachment_see : 'http://i86.servimg.com/u/f86/18/21/41/30/show-a10.png',
+      i_icon_attachment_download : 'http://i86.servimg.com/u/f86/18/21/41/30/downlo10.png',
+
+      // DO NOT TRANSLATE
+      page : 'buttons',
+      submit : 'Save'
+    },
+
+    gallery : {
+      upload_pic : 'http://i35.servimg.com/u/f35/18/21/60/73/upload15.png?color=primary&size=m'
+    }
+
+  },
+
+
+  javascripts : {
+
+    '[FA EDGE] ALL.JS' : {
+      widget_menu_title : '<div class="title module_column_title">Modyul Menu</div>',
+      widget_menu_tooltip : "menu.title = 'I-toggle ang modyul menu';",
+
+      select_theme : 'Pumili ng theme',
+      random_theme : 'Kahit ano ang piliin na theme',
+      default_theme : 'Normal na theme',
+
+      toolbar_search_placeholder : 'Hanapin...',
+
+      quick_nav_title : "title : 'Madaliaang nabigasyon'",
+      quick_nav_tooltip : "tooltip : 'I-toggle ang madaliang nabigasyon'",
+      quick_nav_notif : 'Patalastas',
+      tool_copy_url_tooltip : 'Kopyahin ang BBCode URL',
+      tool_copy_url_prompt : 'Kopyahin ang BBCode URL sa ibaba. (CTRL+C)',
+      tool_admin_panel : 'Kontrols ng Administrasyon',
+      tool_to_top : 'Itaas ng pahina',
+      tool_to_bottom : 'Ibaba ng pahina',
+
+      img_resizer_full_size : 'Ipakita ng lubos ang laki',
+      img_resizer_enlarge : 'Palakihin ang larawan',
+      img_resizer_reduce : 'Paliitin ang larawan',
+      img_resizer_download : 'I-download ang larawan',
+      img_resizer_tooltip : 'I-click ito upang tingnan ng lubos ang larawan',
+
+      fae_update_found : 'Mayroong bagong update ang Forumactif Edge. Kapag ikaw ay handa na, mangyaring tumuloy sa iyong Mga Kontrols ng FAE at i-click ang "Mag check ng bagong update" upang simulan ang proseso ng update.'
+    }
+
+  },
+
+
+  templates : {
+
+    'overall_footer_end.html' : {
+      back_to_top : 'Muling bumalik sa itaas',
+      fae_feedback : 'I-submit ang katugunan',
+      fae_feedback_tooltip : 'Tumulong upang mapabuti ang Forumactif Edge'
+    },
+
+
+    logged_out_reply : {
+      main_title : '<h2 class="lor_maintitle">Gumawa ng isang account o lumagda para mag-iwan ng reply</h2>',
+      main_desc : '<p class="lor_maindesc">Kailangan mo maging isang miyembro upang mag-iwan ng reply.</p>',
+
+      create_account_title : '<h3 class="lor_subtitle">Gumawa ng account</h3>',
+      create_account_desc : '<p class="lor_subdesc">Sumali sa aming community sa pamamagitan ng paggawa ng bagong account. Madali lang ito!</p>',
+      create_account_button : '<a href="/register" class="button1">Gumawa ng bagong account</a>',
+
+      log_in_title : '<h3 class="lor_subtitle">Mag-log in</h3>',
+      log_in_desc : '<p class="lor_subdesc">Mayroon ka nang account? Walang problema, mag-log in dito</p>',
+      log_in_button : '<a href="/login" class="button1">Mag-log in</a>'
+    }
+
+  },
+
+
+  fae_cp : {
+    fae_cp_main_title : 'Mga Kontrols ng Forumactif Edge',
+    fae_cp_desc : 'Itong panel ay nagbibigay daan sa iyo sa pag install, uninstall, at update ng <a href="https://github.com/SethClydesdale/forumactif-edge" target="_blank">Forumactif Edge</a>. Maghanda na upang maranasan ng iyong forum sa isang mas modernong at madaling tablang paraan!',
+    fae_log : 'Mga log',
+    fae_actions : 'Aksyons',
+
+    fae_install : 'I-install',
+    fae_reinstall : 'Muling i-install',
+    fae_install_complete : 'TAPOS!',
+    fae_install_warning : 'Sigurado ka bang gusto mong {INSTALL} ang Forumactif Edge? Ito ay tatanggalin ang iyong kasakuluyang theme at iyong kasakuluyang JavaScripts.\\\n\\\nMangyari sana siguradohin na i-backup ang lahat ng iyong personal content files katulad ng CSS, Templates, at JavaScripts bago magpatuloy. I-click ang "Cancel" kapag ikaw pa ay hindi pa handang i-install ang Forumactif Edge.',
+
+    fae_uninstall : 'I-uninstall',
+    fae_uninstall_warning : 'Sigurado ka bang gusto mong i-uninstall Forumactif Edge? Lahat ng CSS, JavaScript, at Template na pagbabago ay matatangal ; Ang iyong forum ay muling mababalik sa normal na phpbb3 theme.\\\n\\\nMangyari sana siguradohin na i-backup ang lahat ng iyong personal content files katulad ng CSS, Templates, at JavaScripts bago magpatuloy. I-click ang "Cancel" kapag ikaw pa ay hindi pa handang i-uninstall ang Forumactif Edge.',
+
+    fae_update : 'Mag check ng bagong update',
+    fae_update_good : 'Ang Forumactif Edge ay nasa tamang bersyon!',
+    fae_update_check : 'Humahanap ng updates sa Github...',
+    fae_update_prepare : 'Hinahandang ibigay ang instruksyon ng update, mangyaring huwag isara ang tab na ito...',
+    fae_update_start : 'Ang update ay malapit nang magsimula, mangyaring huwag isara ang tab na ito.',
+    fae_update_fetch : 'Kumkuha ng instruksyon sa update para sa bersyon {VERSION_STRING}... ({UPDATE_MIN}/{UPDATE_MAX})',
+    fae_update_notes : 'Mangyaring <a href="{RELEASE_TAG}" target="_blank">i-click ito</a> para malaman ang mga bago sa bersyon {VERSION_STRING}.',
+    fae_update_error : 'Instruksyon ng update para sa bersyon {VERSION_STRING} ay hindi makita. Mangyaring <a href="http://fmdesign.forumotion.com/t700-forumactif-edge-support#13923" target="_blank">gumawa ng support ticket</a> at ibigay itong impormasyon para sa karagdagang tulong.',
+
+    fae_translate : 'Palitan ang wika',
+    fae_translate_same : 'Ang wika ng Forumactif Edge ay {LANG} na talaga. Mangyaring pumili ng ibang wika.',
+    fae_translate_warning : 'Sigurado ka bang gusto mong baguhin ang wika ng Forumactif Edge sa {LANG}?',
+    fae_translate_start : 'Ang pagsasalin ng wika ng Forumactif Edge ay magsisimula na sa ilang sandali. Mangyaring maghintay...',
+    fae_translate_get : 'Kinukuha ang datos ng wikang {LANG}...',
+    fae_translate_loaded : 'Ang datos ng wika ay tapos ng makuha. Ang proseso ng pagsasalin ay sisimulan na ngayon, mangyaring huwag isara ang tab na ito.',
+
+    fae_err_not_founder : 'Tanging <a href="/u1">ang nagtatag</a> ng forum na ito ang may karapatang gumamit ng kontrols para sa FAE. Mangyaring makipag-ugnayan sa kanila o sa kaniya para sa pag-tulong sa pag-install ng Forumactif Edge.'
+
+  }
+
+};

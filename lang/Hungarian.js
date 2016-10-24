08e01572fe5d17b39c14826a214b9230ee8de928
Seth Clydesdale <sethc1995@gmail.com>
2016-10-24 12:46:28 -0400

    Create Hungarian.js

---
+++ lang/Hungarian.js
@@ -0,0 +1,228 @@
+// Hungarian Translation
+// Translation by mimóza (http://fmdesign.forumotion.com/t1058-forumactif-edge-hungarian-translation#22398)
+FAE.lang = {
+
+  language : 'Hungarian',
+
+  css : {
+    offline : 'Kijelentkezve',
+    online : 'Bejelentkezve',
+    total_posts : 'Összes hozzászólás',
+    total_topics : 'Összes téma',
+    total_users : 'Összes tag',
+    record_users : 'Legtöbb felhasználó jelen',
+    board_age : 'Fórum kor (napokban)',
+    newest_member : 'Legújabb tag',
+    hidebox : 'Rejtett:',
+    chatbox_tip : 'A bejelentkezett felhasználók megtekintéséhez húzd ide az egeret.',
+    chatbox_no_away : 'Egy tag sincs távol.',
+    chatbox_no_online : 'Nincs bejelentkezett tag.'
+  },
+
+
+  images : {
+
+    buttons : {
+      i_post : 'https://i97.servimg.com/u/f97/18/21/41/30/new-to10.png?color=primary&size=m',
+      i_reply : 'https://i97.servimg.com/u/f97/18/21/41/30/post-r10.png?color=primary&size=m',
+      i_reply_locked : 'https://i97.servimg.com/u/f97/18/21/41/30/locked10.png?color=secondary&size=m',
+      i_icon_quote : 'https://i97.servimg.com/u/f97/18/21/41/30/quote-10.png?color=primary&size=s',
+      i_icon_multiquote_off : 'https://i97.servimg.com/u/f97/18/21/41/30/multi-10.png?color=primary&size=s',
+      i_icon_multiquote_on : 'https://i97.servimg.com/u/f97/18/21/41/30/multi-10.png?color=secondary&size=s',
+      i_icon_thanks_off : 'https://i97.servimg.com/u/f97/18/21/41/30/thanks10.png?color=primary&size=s',
+      i_icon_thanks_on : 'https://i97.servimg.com/u/f97/18/21/41/30/thanks10.png?color=secondary&size=s',
+      i_icon_edit : 'https://i97.servimg.com/u/f97/18/21/41/30/edit-h10.png?color=primary&size=s',
+      i_icon_delete : 'http://i86.servimg.com/u/f86/18/21/41/30/delete10.png?color=primary&size=s',
+      i_icon_ip : 'http://i86.servimg.com/u/f86/18/21/41/30/ip-f10.png?color=primary&size=s',
+      i_icon_report : 'http://i86.servimg.com/u/f86/18/21/41/30/report10.png?color=primary&size=s',
+      i_icon_report_new : 'http://i86.servimg.com/u/f86/18/21/41/30/report10.png?color=secondary&size=s',
+      i_icon_report_locked : 'http://i86.servimg.com/u/f86/18/21/41/30/report12.png?color=secondary&size=s',
+      i_icon_lock_report : 'http://i86.servimg.com/u/f86/18/21/41/30/report12.png?color=primary&size=s',
+      i_icon_unlock_report : 'http://i86.servimg.com/u/f86/18/21/41/30/report13.png?color=primary&size=s',
+      i_icon_search : FAE.eGIF,
+      i_icon_profile : 'http://i86.servimg.com/u/f86/18/21/41/30/profil12.png?color=primary&size=s',
+      i_icon_www : 'http://i86.servimg.com/u/f86/18/21/41/30/websit10.png?color=primary&size=s',
+      i_icon_email : 'http://i86.servimg.com/u/f86/18/21/41/30/email-10.png?color=primary&size=s',
+      i_icon_pm : 'http://i86.servimg.com/u/f86/18/21/41/30/pm-f11.png?color=primary&size=s',
+      i_icon_fb : 'http://i86.servimg.com/u/f86/18/21/41/30/facebo11.png?color=primary&size=s',
+      i_icon_twitter : 'http://i86.servimg.com/u/f86/18/21/41/30/twitte10.png?color=primary&size=s',
+      i_icon_pinterest : 'http://i86.servimg.com/u/f86/18/21/41/30/pinter10.png?color=primary&size=s',
+      i_icon_aim : 'http://i86.servimg.com/u/f86/18/21/41/30/aim-f10.png?color=primary&size=s',
+      i_icon_icq_add : 'http://i86.servimg.com/u/f86/18/21/41/30/icq-f10.png?color=primary&size=s',
+      i_icon_msnm : 'http://i86.servimg.com/u/f86/18/21/41/30/msn-f10.png?color=primary&size=s',
+      i_icon_yim : 'http://i86.servimg.com/u/f86/18/21/41/30/yahoo-10.png?color=primary&size=s',
+      i_icon_skype : 'http://i86.servimg.com/u/f86/18/21/41/30/skype-10.png?color=primary&size=s',
+      i_icon_online : FAE.eGIF,
+      i_msg_newpost : 'https://i97.servimg.com/u/f97/18/21/41/30/new-pm10.png?color=primary&size=m',
+      i_msg_inbox : FAE.eGIF,
+      i_msg_sentbox : FAE.eGIF,
+      i_msg_outbox : FAE.eGIF,
+      i_msg_savebox : FAE.eGIF,
+      i_icon_ajax_edit : 'http://i86.servimg.com/u/f86/18/21/41/30/edit-f10.png',
+      i_icon_ajax_valid : 'http://i86.servimg.com/u/f86/18/21/41/30/valid-10.png',
+      i_icon_attachment_see : 'http://i86.servimg.com/u/f86/18/21/41/30/show-a10.png',
+      i_icon_attachment_download : 'http://i86.servimg.com/u/f86/18/21/41/30/downlo10.png',
+
+      // DO NOT TRANSLATE
+      page : 'buttons',
+      submit : 'Save'
+    },
+
+    gallery : {
+      upload_pic : 'https://i97.servimg.com/u/f97/18/21/41/30/upload10.png?color=primary&size=m'
+    }
+
+  },
+
+
+  javascripts : {
+
+    '[FA EDGE] ALL.JS' : {
+      widget_menu_title : '<div class="title module_column_title">Modul menü</div>',
+      widget_menu_tooltip : "menu.title = 'Modul menü kapcsoló';",
+
+      select_theme : 'Témaválasztó',
+      random_theme : 'Véletlenszerű téma',
+      default_theme : 'Alapértelmezett téma',
+
+      toolbar_search_placeholder : 'Keresés...',
+
+      quick_nav_title : "title : 'Gyors navigáció'",
+      quick_nav_tooltip : "tooltip : 'Gyors navigáció kapcsoló'",
+      quick_nav_notif : 'Értesítések',
+      tool_copy_url_tooltip : 'BBCode URL másolás',
+      tool_copy_url_prompt : 'Lenti BBCode URL másolása. (CTRL+C)',
+      tool_admin_panel : 'Admin Panel',
+      tool_to_top : 'Oldal teteje',
+      tool_to_bottom : 'Oldal alja',
+
+      img_resizer_full_size : 'Teljes méret mutatása',
+      img_resizer_enlarge : 'Kép nagyítás',
+      img_resizer_reduce : 'Kép kicsinyítés',
+      img_resizer_download : 'Kép letöltés',
+      img_resizer_tooltip : 'Teljes mérethez kattints',
+
+      fae_update_found : 'Frissítés található a Forumactif Edgehez. Abban az esetben, ha készen állsz az aktualizálásra, a FAE kezelőpanelnél kattints a "Frissítések keresése" opcióra és a frissítés kezdetét veszi.'
+    },
+
+
+    '[FA EDGE] TOPICS.JS' : {
+      thanked_message : '<i class="fa fa-thumbs-up"></i> {USERNAME} köszönömben részesült a téma indítótól !'
+    }
+
+  },
+
+
+  templates : {
+    'overall_header.html' : {
+      announcements : '<div id="fa_ticker_title" class="title">Közlemények</div>'
+    },
+
+
+    'overall_footer_end.html' : {
+      back_to_top : 'Vissza a tetejére',
+      fae_feedback : 'Visszajelzés küldése',
+      fae_feedback_tooltip : 'Járulj hozzá a Forumactif Edge fejlesztéséhez'
+    },
+
+
+    logged_out_reply : {
+      main_title : '<h2 class="lor_maintitle">A válaszoláshoz készíts egy fiókot vagy jelentkezz be.</h2>',
+      main_desc : '<p class="lor_maindesc">A válaszoláshoz tagság szükséges.</p>',
+
+      create_account_title : '<h3 class="lor_subtitle">Fiókkészítés</h3>',
+      create_account_desc : '<p class="lor_subdesc">Csatlakozz a közösségünkhöz egy fiók létrehozásával. Könnyű!</p>',
+      create_account_button : '<a href="/register" class="button1">Készíts egy új fiókot</a>',
+
+      log_in_title : '<h3 class="lor_subtitle">Belépés</h3>',
+      log_in_desc : '<p class="lor_subdesc">Már rendelkezel egy fiókkal? Jelentkezz be itt.</p>',
+      log_in_button : '<a href="/login" class="button1">Belépés</a>'
+    }
+
+  },
+
+
+  fae_cp : {
+    fae_cp_main_title : 'Forumactif Edge Kezelőpanel',
+    fae_cp_desc : 'Ennek a panelnek a segítségével telepítheted, eltávolíthatod, frissítheted a <a href="https://github.com/SethClydesdale/forumactif-edge" target="_blank">Forumactif Edge-t</a>. Készülj fel egy modernebb és responsive-abb fórum élményre!',
+    fae_log : 'Napló',
+    fae_actions : 'Tevékenységek',
+
+    fae_install : 'Telepítés',
+    fae_reinstall : 'Újratelepítés',
+    fae_install_complete : 'KÉSZ!',
+    fae_install_warning : 'Biztos, hogy a Forumactif Edge-dzsel kapcsolatban a(z) {INSTALL} opciót választod? A jelenlegi témád felül lesz írva, továbbá a JavaScrptjeid törölve lesznek.\\\n\\\nKérlek készíts biztonsági másolatot minden személyes tartalomról, beleértve a CSS kiegészítéseidről, sablonjaidról és a Java Scriptekről, mielőtt folytatnád a folyamatot.\\\nKattints a "Cancel" - "Mégsem" opcióra, amennyiben nem állsz készen a Forumactif Edge telepítésére.',
+
+    fae_uninstall : 'Eltávolítás',
+    fae_uninstall_warning : 'Biztos vagy benne, hogy el szeretnéd távolítani a Forumactif Edget? Az összes korábbi CSS, JavaScript és sablon változtatás törölve lesz ; A fórum vissza lesz állítva az alapértelmezett phpbb3 témára.\\\n\\\nKattints a "Cancel" - "Mégsem" opcióra, amennyiben nem állsz készen a Forumactif Edge eltávolítására.',
+
+    fae_update : 'Frissítések keresése.',
+    fae_update_good : 'A Forumactif Edge naprakész!',
+    fae_update_check : 'Frissítések keresése Githubon...',
+    fae_update_prepare : 'Felkészülés az utasítások frissítésére, ne zárd be ezt a lapot...',
+    fae_update_start : 'A frissítések mindjárt elkezdődnek, ne zárd be ezt a lapot.',
+    fae_update_fetch : 'Frissítési utasítások szerzése a következő verziókhoz: {VERSION_STRING}... ({UPDATE_MIN}/{UPDATE_MAX})',
+    fae_update_notes : '<a href="{RELEASE_TAG}" target="_blank">Kattints ide</a> a következő verzióban található változások megtekintéséhez ({VERSION_STRING}).',
+    fae_update_error : 'Frissítés információ a {VERSION_STRING} verzióhoz nem található. <a href="http://fmdesign.forumotion.com/t700-forumactif-edge-support#13923" target="_blank">Indíts egy új témát</a> és biztosítsd az összes rendelkezésedre álló információt, hogy segíteni tudjunk.',
+
+    fae_translate : 'Nyelvváltoztatás',
+    fae_translate_same : 'A Forumactif Edge már a kiválasztott {LANG} nyelvet használja. Kérlek válassz egy másik nyelvet.',
+    fae_translate_warning : 'Biztos vagy benne, hogy a Forumactif Edge nyelvét meg szeretnéd változtatni {LANG} nyelvre?',
+    fae_translate_start : 'A Forumactif Edge fordítása pillanatokon belül elkezdődik. Várj türelmesen.',
+    fae_translate_get : '{LANG} nyelv adatainak beszerzése...',
+    fae_translate_loaded : 'A nyelvhez szükséges adatok betöltődtek. A fordítás mindjárt elkezdődik, kérlek, ne zárd be ezt az oldalt.',
+
+    fae_err_not_founder : 'Csak és kizárólag <a href="/u1">az alapító</a> használhatja ezt a kezelőpanelt. Lépj kapcsolatba velük a Forumactif Edge telepítéséhez.',
+
+    left : 'Bal',
+    center : 'Közép',
+    right : 'Jobb',
+    yes : 'Igen',
+    no : 'Nem',
+    help : 'Segítség!',
+
+    general_settings : {
+      title : 'Általános beállítások',
+      fae_gen_desc : 'Ez a részleg teszi lehetővé a Forumactif Edge általános beállításait.',
+
+      'fae_label-width' : 'Fórum szélesség :',
+      'fae_gen_tip-width' : 'Húzd a csúszkát a fórum szélességének állításához.',
+
+      'fae_label-navbar' : 'Navigációs sáv pozíciója :',
+      'fae_gen_tip-navbar' : 'Navigációs sáv linkjeinek pozíciója.',
+
+      'fae_label-logo' : 'Logó pozíció :',
+      'fae_gen_tip-logo' : 'Fórum logó pozíciója.',
+
+      'fae_label-profile' : 'Profil pozíció :',
+      'fae_gen_tip-profile' : 'Hozzászóló profiljának elhelyezkedése.',
+
+      fae_update_general : 'Változtatások mentése',
+      fae_gen_updating : 'Általános beállítások frissítése..',
+      fae_gen_updated : 'Az általános beállítások frissítése sikeresen megtörtént !'
+    },
+
+
+    theme_management : {
+      title : 'Téma kezelés',
+      fae_theme_desc : 'Ez a részleg teszi lehetővé az Forumactif Edge alapértelmezett témájának és színeinek kezelését új téma importálásával a Github gyűjteményből.',
+
+      'fae_label-theme' : 'Válassz egy témát :',
+      'fae_theme_tip-theme' : 'Válaszd ki az importálandó téma típusát.',
+
+      'fae_label-color' : 'Válassz egy színt :',
+      'fae_theme_tip-color' : 'Válaszd ki a kiválasztott téma alapértelmezett színét.',
+
+      'fae_label-minify' : 'A stíluslap rövidítése :',
+      'fae_theme_tip-minify' : 'A rövidített stíluslapok kevesebb helyet foglalnak el, azonban a szerkesztésük nehézkes. Amennyiben változtatásokat tervezel a stíluslapban , inkább válaszd a "Nem" opciót".',
+
+      'fae_label-rtl' : 'Jobbról balra :',
+      'fae_theme_tip-rtl' : 'A téma szövegének irányát megváltoztatja balról jobbra helyett, jobbról balra.',
+
+      fae_import_theme : 'Téma importálása',
+      fae_import_confirm : 'Biztos vagy benne, hogy importálni szeretnéd a következő témát: "{THEME_NAME}" ?\\\n\\\nA stíluslap felül lesz írva az új témával, amennyiben meg szeretnéd tartani a jelenlegi változtatásaidat készíts biztonsági másolatot. Kattints a "Mégsem" opcióra, ha nem állsz készen az új téma importálására.'
+    }
+
+  }
+
+};

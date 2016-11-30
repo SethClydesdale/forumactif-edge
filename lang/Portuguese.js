a217ba686dc224afb724a03eb931fdf54dfcce4c
Seth Clydesdale <sethc1995@gmail.com>
2016-11-30 12:02:26 -0500

    Create Portuguese.js

---
+++ lang/Portuguese.js
@@ -0,0 +1,228 @@
+// Portuguese Translation
+// Translation by Harleen (http://fmdesign.forumotion.com/t1141-forumactif-edge-portuguese-translation-brazilian#27123)
+FAE.lang = {
+
+  language : 'Português',
+
+  css : {
+    offline : 'DESCONECTADO',
+    online : 'CONECTADO',
+    total_posts : 'Total de Mensagens',
+    total_topics : 'Total de Tópicos',
+    total_users : 'Total de Membros',
+    record_users : 'Recorde de Membros Online',
+    board_age : 'Idade do Fórum (dias)',
+    newest_member : 'Novo Membro',
+    hidebox : 'Esconder:',
+    chatbox_tip : 'Passe o mouse sobre para ver quem está on-line.',
+    chatbox_no_away : 'Nenhum membro ausente...',
+    chatbox_no_online : 'Nenhum membro online.'
+  },
+
+
+  images : {
+
+    buttons : {
+      i_post : 'https://i37.servimg.com/u/f37/18/21/41/30/new-to10.png?color=primary&size=m',
+      i_reply : 'https://i37.servimg.com/u/f37/18/21/41/30/post-r10.png?color=primary&size=m',
+      i_reply_locked : 'https://i37.servimg.com/u/f37/18/21/41/30/locked10.png?color=secondary&size=m',
+      i_icon_quote : 'https://i37.servimg.com/u/f37/18/21/41/30/quote-10.png?color=primary&size=s',
+      i_icon_multiquote_off : 'https://i37.servimg.com/u/f37/18/21/41/30/multi-10.png?color=primary&size=s',
+      i_icon_multiquote_on : 'https://i37.servimg.com/u/f37/18/21/41/30/multi-10.png?color=secondary&size=s',
+      i_icon_thanks_off : 'https://i37.servimg.com/u/f37/18/21/41/30/thanks10.png?color=primary&size=s',
+      i_icon_thanks_on : 'https://i37.servimg.com/u/f37/18/21/41/30/thanks10.png?color=secondary&size=s',
+      i_icon_edit : 'https://i37.servimg.com/u/f37/18/21/41/30/edit-p10.png?color=primary&size=s',
+      i_icon_delete : 'http://i86.servimg.com/u/f86/18/21/41/30/delete10.png?color=primary&size=s',
+      i_icon_ip : 'http://i86.servimg.com/u/f86/18/21/41/30/ip-f10.png?color=primary&size=s',
+      i_icon_report : 'http://i86.servimg.com/u/f86/18/21/41/30/report10.png?color=primary&size=s',
+      i_icon_report_new : 'http://i86.servimg.com/u/f86/18/21/41/30/report10.png?color=secondary&size=s',
+      i_icon_report_locked : 'http://i86.servimg.com/u/f86/18/21/41/30/report12.png?color=secondary&size=s',
+      i_icon_lock_report : 'http://i86.servimg.com/u/f86/18/21/41/30/report12.png?color=primary&size=s',
+      i_icon_unlock_report : 'http://i86.servimg.com/u/f86/18/21/41/30/report13.png?color=primary&size=s',
+      i_icon_search : FAE.eGIF,
+      i_icon_profile : 'http://i86.servimg.com/u/f86/18/21/41/30/profil12.png?color=primary&size=s',
+      i_icon_www : 'http://i86.servimg.com/u/f86/18/21/41/30/websit10.png?color=primary&size=s',
+      i_icon_email : 'http://i86.servimg.com/u/f86/18/21/41/30/email-10.png?color=primary&size=s',
+      i_icon_pm : 'http://i86.servimg.com/u/f86/18/21/41/30/pm-f11.png?color=primary&size=s',
+      i_icon_fb : 'http://i86.servimg.com/u/f86/18/21/41/30/facebo11.png?color=primary&size=s',
+      i_icon_twitter : 'http://i86.servimg.com/u/f86/18/21/41/30/twitte10.png?color=primary&size=s',
+      i_icon_pinterest : 'http://i86.servimg.com/u/f86/18/21/41/30/pinter10.png?color=primary&size=s',
+      i_icon_aim : 'http://i86.servimg.com/u/f86/18/21/41/30/aim-f10.png?color=primary&size=s',
+      i_icon_icq_add : 'http://i86.servimg.com/u/f86/18/21/41/30/icq-f10.png?color=primary&size=s',
+      i_icon_msnm : 'http://i86.servimg.com/u/f86/18/21/41/30/msn-f10.png?color=primary&size=s',
+      i_icon_yim : 'http://i86.servimg.com/u/f86/18/21/41/30/yahoo-10.png?color=primary&size=s',
+      i_icon_skype : 'http://i86.servimg.com/u/f86/18/21/41/30/skype-10.png?color=primary&size=s',
+      i_icon_online : FAE.eGIF,
+      i_msg_newpost : 'https://i37.servimg.com/u/f37/18/21/41/30/new-pm10.png?color=primary&size=m',
+      i_msg_inbox : FAE.eGIF,
+      i_msg_sentbox : FAE.eGIF,
+      i_msg_outbox : FAE.eGIF,
+      i_msg_savebox : FAE.eGIF,
+      i_icon_ajax_edit : 'http://i86.servimg.com/u/f86/18/21/41/30/edit-f10.png',
+      i_icon_ajax_valid : 'http://i86.servimg.com/u/f86/18/21/41/30/valid-10.png',
+      i_icon_attachment_see : 'http://i86.servimg.com/u/f86/18/21/41/30/show-a10.png',
+      i_icon_attachment_download : 'http://i86.servimg.com/u/f86/18/21/41/30/downlo10.png',
+
+      // DO NOT TRANSLATE
+      page : 'buttons',
+      submit : 'Save'
+    },
+
+    gallery : {
+      upload_pic : 'https://i37.servimg.com/u/f37/18/21/41/30/upload10.png?color=primary&size=m'
+    }
+
+  },
+
+
+  javascripts : {
+
+    '[FA EDGE] ALL.JS' : {
+      widget_menu_title : '<div class="title module_column_title">Widget Menu</div>',
+      widget_menu_tooltip : "menu.title = 'Toggle widget menu';",
+
+      select_theme : 'Selecionar um tema',
+      random_theme : 'Tema Aleatório',
+      default_theme : 'Tema Padrão',
+
+      toolbar_search_placeholder : 'Buscar...',
+
+      quick_nav_title : "title : 'Navegação Rápida'",
+      quick_nav_tooltip : "tooltip : 'Alternar Navegação Rápida'",
+      quick_nav_notif : 'Notificações',
+      tool_copy_url_tooltip : 'Copiar BBCode URL',
+      tool_copy_url_prompt : 'Copiar o BBCode URL abaixo. (CTRL+C)',
+      tool_admin_panel : 'Painel Admin',
+      tool_to_top : 'Início da Página',
+      tool_to_bottom : 'Fim da Página',
+
+      img_resizer_full_size : 'Mostrar tamanho grande',
+      img_resizer_enlarge : 'Aumentar imagem',
+      img_resizer_reduce : 'Reduzir imagem',
+      img_resizer_download : 'Baixar imagem',
+      img_resizer_tooltip : 'Clique para ver completo',
+
+      fae_update_found : 'Foi encontrada uma atualização para FA edge. Vá no painel de controle da FAE e clique em "Verificar atualizações" para obter a versão mais recente.'
+    },
+
+
+    '[FA EDGE] TOPICS.JS' : {
+      thanked_message : '<div class="thanked-message"><i class="fa fa-thumbs-up"></i> {USERNAME} foi agradecido por iniciar o tema!</div>'
+    }
+
+  },
+
+
+  templates : {
+    'overall_header.html' : {
+      announcements : '<div id="fa_ticker_title" class="title">Anúncios</div>'
+    },
+
+
+    'overall_footer_end.html' : {
+      back_to_top : 'Voltar ao topo',
+      fae_feedback : 'Enviar Feedback',
+      fae_feedback_tooltip : 'Ajude a melhorar o Forumactif Edge'
+    },
+
+
+    logged_out_reply : {
+      main_title : '<h2 class="lor_maintitle">Criar uma conta ou fazer login para responder</h2>',
+      main_desc : '<p class="lor_maindesc">Você deve ser um membro para responder.</p>',
+
+      create_account_title : '<h3 class="lor_subtitle">Crie uma Conta</h3>',
+      create_account_desc : '<p class="lor_subdesc">Junte-se a nossa comunidade! É fácil!</p>',
+      create_account_button : '<a href="/register" class="button1">Criar nova conta</a>',
+
+      log_in_title : '<h3 class="lor_subtitle">Logar</h3>',
+      log_in_desc : '<p class="lor_subdesc">Já é um membro? Não se preocupe, clique aqui para entrar.</p>',
+      log_in_button : '<a href="/login" class="button1">Logar</a>'
+    }
+
+  },
+
+
+  fae_cp : {
+    fae_cp_main_title : 'Painel de Controle do Forumactif Edge',
+    fae_cp_desc : 'Este painel permite-lhe instalar, desinstalar e atualizar o <a href="https://github.com/SethClydesdale/forumactif-edge" target="_blank">Forumactif Edge</a>. Prepare-se para experimentar o seu fórum de uma forma mais moderna e ágil!',
+    fae_log : 'Log',
+    fae_actions : 'Ações',
+
+    fae_install : 'Instalar',
+    fae_reinstall : 'Reinstalar',
+    fae_install_complete : 'CONCLUÍDO!',
+    fae_install_warning : 'Tem certeza de que deseja {INSTALL} Forumactif Edge? Isso substituirá seu tema atual e excluirá seus JavaScript atuais.\\\n\\\nPor favor, certifique-se de fazer backup de todos os arquivos pessoais como CSS, Templates, e JavaScripts antes de continuar. Clique em "Cancelar" se não está pronto para instalar Forumactif Edge.',
+
+    fae_uninstall : 'Desinstalar',
+    fae_uninstall_warning : 'Tem certeza de que deseja desinstalar? O CSS, JavaScript e temas instalados serão excluídos; Este fórum irá retornar para a versão padrão do phpBB3.\\\n\\\nPor favor, certifique-se de fazer backup de todos os arquivos pessoais como CSS, Templates, e JavaScripts antes de continuar. Clique em "Cancelar" se você não deseja desinstalar Forumactif Edge ainda.',
+
+    fae_update : 'Verificar atualizações',
+    fae_update_good : 'O Forumactif Edge está atualizado!',
+    fae_update_check : 'Procurar Atualizações no Github...',
+    fae_update_prepare : 'Comparação de versões, por favor, não feche a página.',
+    fae_update_start : 'As atualizações estão prestes a começar, por favor, não feche esta aba.',
+    fae_update_fetch : 'Colhendo instruções de atualizações para a versão {VERSION_STRING}... ({UPDATE_MIN}/{UPDATE_MAX})',
+    fae_update_notes : 'Por Favor <a href="{RELEASE_TAG}" target="_blank">Clique aqui</a> para ver as alterações feitas na versão',
+    fae_update_error : 'Instruções de atualização para a versão {VERSION_STRING} não pôde ser encontrado. Por Favor <a href="http://fmdesign.forumotion.com/t700-forumactif-edge-support#13923" target="_blank">abrir um novo tópico de suporte</a> e fornecer esta informação para maior assistência.',
+
+    fae_translate : 'Mudar o Idioma',
+    fae_translate_same : 'O Forumactif Edge já está em {LANG}. Por Favor escolha outro idioma',
+    fae_translate_warning : 'Tem certeza de que deseja alterar o idioma do Forumactif Edge para {LANG} ?',
+    fae_translate_start : 'A tradução do Forumactif Edge começara em breve. Por Favor aguarde..',
+    fae_translate_get : 'Recebendo {LANG} dados do idioma...',
+    fae_translate_loaded : 'Os dados do idioma foram carregados. O processo de tradução irá começar, por favor, não feche esta guia.',
+
+    fae_err_not_founder : 'Somente <a href="/u1">o fundador</a> pode usar este painel de controle. Entre em contato com eles para obter ajuda na instalação do Forumactif Edge.',
+
+    left : 'Esquerda',
+    center : 'Centralizar',
+    right : 'Direita',
+    yes : 'Sim',
+    no : 'Não',
+    help : 'Ajuda!',
+
+    general_settings : {
+      title : 'Configurações Gerais',
+      fae_gen_desc : 'Esta seção permite-lhe gerir as definições gerais do Forumactif Edge.',
+
+      'fae_label-width' : 'Tamanho do Forum :',
+      'fae_gen_tip-width' : 'Arraste o botão deslizante para ajustar a largura do fórum.',
+
+      'fae_label-navbar' : 'Posição da barra de navegação :',
+      'fae_gen_tip-navbar' : 'Posição dos links na barra de navegação.',
+
+      'fae_label-logo' : 'Posição do Logo :',
+      'fae_gen_tip-logo' : 'Posição do logo do fórum.',
+
+      'fae_label-profile' : 'Posição do perfil :',
+      'fae_gen_tip-profile' : 'Posição do perfil nos tópicos.',
+
+      fae_update_general : 'Salvar Alterações',
+      fae_gen_updating : 'Atualizando as configurações gerais...',
+      fae_gen_updated : 'As definições gerais foram atualizados com sucesso !'
+    },
+
+
+    theme_management : {
+      title : 'Gestão Tema',
+      fae_theme_desc : 'Esta seção permite gerenciar o tema e as cores padrão para Forumactif Edge importando um novo tema a partir do repositório Github.',
+
+      'fae_label-theme' : 'Escolha um tema :',
+      'fae_theme_tip-theme' : 'Selecione o tipo de tema que você deseja importar.',
+
+      'fae_label-color' : 'Selecionar uma cor :',
+      'fae_theme_tip-color' : 'Selecione a cor padrão para o tema escolhido.',
+
+      'fae_label-minify' : 'Minimizar o CSS :',
+      'fae_theme_tip-minify' : 'Minimize a folha de estilo, não é ideal para edição. Se você quiser editar a folha de estilo do tema selecionado, escolha "Não" em vez disso.',
+
+      'fae_label-rtl' : 'Direita para esquerda :',
+      'fae_theme_tip-rtl' : 'Alterar a direção do texto: da direita para a esquerda ou da esquerda para a direita.',
+
+      fae_import_theme : 'Importar o tema',
+      fae_import_confirm : 'Tem certeza que quer importar o tema? "{THEME_NAME}" ?\\\n\\\nCertifique-se de fazer backup de sua folha de estilo atual se quiser mantê-la, pois ela será substituída quando o novo tema for importado. Escolha "Cancelar" se não estiver pronto para importar um novo tema.'
+    }
+
+  }
+
+};

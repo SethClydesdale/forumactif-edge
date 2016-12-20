5a489683697bc98f3fa0c92c337c3539930f3cb4
Seth Clydesdale <sethc1995@gmail.com>
2016-12-20 13:44:03 -0500

    Update Arabic.js

--- lang/Arabic.js
+++ lang/Arabic.js
@@ -1,22 +1,22 @@
 // Arabic Translation
-// Translation by omarpop23, Michael_vx, Gin NeOs (http://fmdesign.forumotion.com/t740-forumactif-edge-arabic-translation#14583)
+// Translation by omarpop23, Michael_vx, Gin NeOs, amghidh (http://fmdesign.forumotion.com/t740-forumactif-edge-arabic-translation#14583)
 FAE.lang = {
 
   language : 'العربية',
 
   css : {
-    offline : 'غير مُتصل',
-    online : 'مُتصل',
-    total_posts : 'اجمالي المساهمات',
-    total_topics : 'اجمالي المواضيع',
-    total_users : 'اجمالي الأعضاء',
-    record_users : 'أكثر الأعضاء تواجداً',
-    board_age : 'عُمر المنتدى (بلأيام)',
-    newest_member : 'آخر عُضو مُسجل',
-    hidebox : 'مخفي:',
-    chatbox_tip : 'مرر اللفأرة لترى المتصل و الغائب.',
-    chatbox_no_away : 'غائب.',
-    chatbox_no_online : 'مُتصل.'
+    offline : 'غير متصل',
+    online : 'متصل',
+    total_posts : 'إجمالي المساهمات',
+    total_topics : 'إجمالي المواضيع',
+    total_users : 'إجمالي عدد الأعضاء',
+    record_users : 'أكبر عدد من المتصلين',
+    board_age : 'عمر المنتدى (بالأيام)',
+    newest_member : 'آخر عضو مسجّل',
+    hidebox : 'محتوى مخفي:',
+    chatbox_tip : 'ضع مؤشر الفأرة هنا لمعاينة المتصلين.',
+    chatbox_no_away : 'لا يوجد حاليا أعضاء غائبين...',
+    chatbox_no_online : 'لا يوجد حاليا عضو متصل...'
   },
 
 
@@ -81,33 +81,33 @@ FAE.lang = {
       widget_menu_title : '<div class="title module_column_title">قائمة جانبية</div>',
       widget_menu_tooltip : "menu.title = 'قائمة جانبية';",
 
-      select_theme : 'لونك المُفضل',
+      select_theme : 'الرجاء إختيار لون',
       random_theme : 'لون عشوائي',
-      default_theme : 'اللون الأفتراضي',
+      default_theme : 'اللون الافتراضي',
 
-      toolbar_search_placeholder : 'بحــث...',
+      toolbar_search_placeholder : 'بحث...',
 
-      quick_nav_title : "title : 'الأنتقال السريع'",
-      quick_nav_tooltip : "tooltip : 'قائمة الأنتقال السريع'",
-      quick_nav_notif : 'الأشعارات',
-      tool_copy_url_tooltip : 'نسخ عنوان BBCode الـ للصفحة',
-      tool_copy_url_prompt : 'أنسخ عنوان الـ BBCode. (CTRL+C)',
-      tool_admin_panel : 'لوحة الأدارة',
-      tool_to_top : 'أعلي المنتدى',
-      tool_to_bottom : 'أسفل المنتدى',
+      quick_nav_title : "title : 'الإنتقال السريع'",
+      quick_nav_tooltip : "tooltip : 'الإنتقال السريع'",
+      quick_nav_notif : '	الإشعارات',
+      tool_copy_url_tooltip : 'نسخ رابط BBCode',
+      tool_copy_url_prompt : 'إنسخ رابط الـBBCode التالي (CTRL+C)',
+      tool_admin_panel : 'لوحة التحكم',
+      tool_to_top : 'أعلى الصفحة',
+      tool_to_bottom : 'أسفل الصفحة',
 
-      img_resizer_full_size : 'عرض المقاسات الاصليه',
+      img_resizer_full_size : 'المعاينة بالمقاسات الاصلية',
       img_resizer_enlarge : 'تكبير الصورة',
       img_resizer_reduce : 'تصغير الصورة',
       img_resizer_download : 'تحميل الصورة',
-      img_resizer_tooltip : 'أضغط هنا للعرض الطبيعي للصورة',
+      img_resizer_tooltip : '	إضغط لمعاينة الصورة كاملة',
 
-      fae_update_found : 'تهانينا يتوفر تحديث جديد من نسخة المنتدي .'
+      fae_update_found : 'لقد تم إيجاد تحديث لنسختك من فوروماكتيف إيدج Forumactif EDGE... عندما تكون جاهزا، الرجاء التوجه نحو لوحة التحكّم بالنسخة و التحديث بالضغط على "البحث عن تحديثات"'
     },
 
 
     '[FA EDGE] TOPICS.JS' : {
-      thanked_message : '<div class="thanked-message"><i class="fa fa-thumbs-up"></i> {USERNAME} has been thanked by the topic starter !</div>'
+      thanked_message : '<div class="thanked-message"><i class="fa fa-thumbs-up"></i> {USERNAME} قد شكر فاتح الموضوع!</div>'
     }
 
   },
@@ -115,14 +115,14 @@ FAE.lang = {
 
   templates : {
     'overall_header.html' : {
-      announcements : '<div id="fa_ticker_title" class="title">Announcements</div>'
+      announcements : '<div id="fa_ticker_title" class="title">الإعلانات</div>'
     },
 
 
     'overall_footer_end.html' : {
-      back_to_top : 'ألي أعلي',
-      fae_feedback : 'التغذية العكسية',
-      fae_feedback_tooltip : 'ساعدنا في تطوير نسخة المنتدى'
+      back_to_top : 'العودة إلى الأعلى',
+      fae_feedback : 'إرسال رأيك الشخصي',
+      fae_feedback_tooltip : 'ساهم في تطوير Forumactif EDGE'
     },
 
 
@@ -143,84 +143,84 @@ FAE.lang = {
 
 
   fae_cp : {
-    fae_cp_main_title : 'لوحة التحكم الخاصة بنسخة Forumactif Edge المتطورة.',
-    fae_cp_desc : 'لوحة التحكم هذه تمكنك من ثبيت/حذف نسخة <a href="https://github.com/SethClydesdale/forumactif-edge" target="_blank">Forumactif Edge</a>. كن مستعد لتحصل علي نسخة حديثة و متطورة و تدعم نسخة الجوال!',
+    fae_cp_main_title : 'لوحة التحكم بنسخة Forumactif EDGE',
+    fae_cp_desc : 'هذه اللوحة تسمح لك بثبيت، مسح و تحديث نسختك من <a href="https://github.com/SethClydesdale/forumactif-edge" target="_blank">Forumactif Edge</a>. كن مستعدا لإستكشاف منتداك بحلة جديدة متطورة و سهلة التصفح!',
     fae_log : 'السجل',
     fae_actions : 'العمليات',
 
     fae_install : 'تثبيت',
-    fae_reinstall : 'اعادة التثبيت',
-    fae_install_complete : 'اكتمل التثبيت!',
-    fae_install_warning : 'هل انت متأكد انك تريد  {INSTALL} نسخة Forumactif Edge المتطورة؟ سوف يتم حذف التصميم الحالي و اكواد الجافا. من فضلك تأكد بانك تملك نسخة احتياطية من ملفاتك الشخصية الخاصة بالتصميم الحالي مثل القوالب و اكواد الجافا و الاكواد الانسيابية اضغط علي الغاء اذا لم تكن مستعد للتثبيت.',
+    fae_reinstall : 'إعادة التثبيت',
+    fae_install_complete : 'إنتهى التثبيت بنجاح!',
+    fae_install_warning : 'هل أنت متأكد من رغبتك {INSTALL} Forumactif Edge؟ هذا سيؤدّي إلى حذف التصميم الخاص بك و أكواد الجافا سكريبت كذلك!الرجاء التأكد من وضع نسخة إحتياطية من ملفاتك الشخصية (أكواد انسيابية، تومبيلايت و أكواد الجافا سكريبت) قبل المواصلة. الرجاء الضغط على "إلغاء" في حال عدم إستعدادك الكامل لتثبيت Forumactif Edge',
 
-    fae_uninstall : 'حذف',
-    fae_uninstall_warning : 'هل انت متأكد انك تريد حذف نسخة Forumactif Edge المتطورة؟ سيتم حذف جميع اكواد الجافا و القوالب و الاكواد الانسيابيه، سيعود التصميم الافتراضي للنسخة الثالثة.من فضلك تأكد بانك تملك نسخة احتياطية من ملفاتك الشخصية الخاصة بالتصميم الحالي مثل القوالب و اكواد الجافا و الاكواد الانسيابية اضغط علي الغاء اذا لم تكن مستعد لعملية الحذف.',
+    fae_uninstall : 'حذف النسخة',
+    fae_uninstall_warning : 'هل أنت متأكد من رغبتك حذف نسختك من Forumactif Edge؟ هذا سيؤدي إلى حذف جميع الأكواد الانسيابية، الجافاسكريبت و قوالب التومبلايت و سيتم إرجاع منتداك إلى التصميم الافتراضي لـPHPBB 3الرجاء التأكد من وضع نسخة إحتياطية من ملفاتك الشخصية (أكواد انسيابية، تومبيلايت و أكواد الجافا سكريبت) قبل المواصلة. الرجاء الضغط على "إلغاء" في حال تراجعت عن رأيك في مسح نسختك من Forumactif Edge',
 
-    fae_update : 'التحقق من وجود تحديثات',
-    fae_update_good : 'لديك آخر اصدار من نسخة Forumactif Edge المتطورة!',
-    fae_update_check : 'جاري فحص التحديثات علي Github...',
-    fae_update_prepare : 'جاري التحضير لجلب معلومات التحديث ، من فضلك لا تغلق هذه النافذه.',
-    fae_update_start : 'التحديثات علي وشك التثبيت، من فضلك لا تغلق هذه النافذة.',
-    fae_update_fetch : 'جاري تحميل معلومات لنسخة {VERSION_STRING}... ({UPDATE_MIN}/{UPDATE_MAX})',
-    fae_update_notes : 'من فضلك <a href="{RELEASE_TAG}" target="_blank">اضغط هنا</a> لعرض التغيرات الجديدة {VERSION_STRING}.',
-    fae_update_error : 'معلومات التحديث لنسخة {VERSION_STRING} لا يمكن الوصول اليها. من فضلك <a href="http://fmdesign.forumotion.com/t700-forumactif-edge-support#13923" target="_blank">فتح موضوع جديد</a> و اخبرنا بهذه المعلومات لتقديم المزيد من المساعدة.',
+    fae_update : 'البحث عن تحديثات',
+    fae_update_good : 'أنت تمتلك حاليا النسخة الاخيرة من Forumactif Edge!',
+    fae_update_check : 'البحث عن تحديثات في Github...',
+    fae_update_prepare : 'يتم حاليا التحضير للتحديث... الرجاء عدم إقفال النافذة!',
+    fae_update_start : 'سيبدأ التحديث خلال لحظات... الرجاء عدم إقفال النافدة!',
+    fae_update_fetch : 'الحصول على تعليمات التحديث للاصدار {VERSION_STRING}... ({UPDATE_MIN}/{UPDATE_MAX})',
+    fae_update_notes : 'الرجاء <a href="{RELEASE_TAG}" target="_blank">الضغط هنا</a> لمعاينة التطورات التي أجريت للنسخة {VERSION_STRING}.',
+    fae_update_error : 'لم يتم إيجاد تعليمات التحديث للإصدار {VERSION_STRING} . الرجاء <a href="http://fmdesign.forumotion.com/t700-forumactif-edge-support#13923" target="_blank">فتح تذكرة دعم</a> و إخبارنا بهذا الخلل لنتمكن من مساعدتك',
 
-    fae_translate : 'تغير اللغة',
-    fae_translate_same : 'نسخة Forumactif Edge المتطورة {LANG}. من فضلك اختار لغة آخرى.',
-    fae_translate_warning : 'هل انت متأكد انك تريد تغير لغة نسخة  Forumactif Edge الي {LANG} ?',
-    fae_translate_start : 'سوف تبدأ ترجمة نسخة Forumactif Edge رجاء الانتظار...',
-    fae_translate_get : 'جاري الحصول علي بيانات اللغة  {LANG} ...',
-    fae_translate_loaded : 'تم تحميل بيانات اللغة . عملية الترجمة علي وشك البدأ ، منفضلك لا تغلق هذه النافذة.',
+    fae_translate : 'تغيير اللغة',
+    fae_translate_same : 'منتداك يستعمل فعلا اللغة {LANG}. الرجاء إختيار لغة أخرى.',
+    fae_translate_warning : 'هل أنت واثق من رغبتك تحويل لغة منتداك إلى {LANG} ؟',
+    fae_translate_start : 'سيتم ترجمة منتداك خلال لحظات. الرجاء الانتظار...',
+    fae_translate_get : 'الحصول على بيانات اللغة {LANG}',
+    fae_translate_loaded : 'تم تحميل بيانات اللغة. سيتم الترجمة خلال لحظات... الرجاء عدم إقفال هذه النافذة!',
 
-    fae_err_not_founder : 'وحده  <a href="/u1">منشئ المنتدي </a> القادر علي استخدام هذه اللوحة.',
+    fae_err_not_founder : 'فقط <a href="/u1">مؤسس المنتدى</a> يمكنه إستعمال هذه اللوحة. ',
 
-    left : 'Left',
-    center : 'Center',
-    right : 'Right',
-    yes : 'Yes',
-    no : 'No',
-    help : 'Help!',
+    left : 'يسار',
+    center : 'وسط',
+    right : 'يمين',
+    yes : 'نعم',
+    no : 'لا',
+    help : 'أحتاج إلى مساعدة!',
 
     general_settings : {
-      title : 'General Settings',
-      fae_gen_desc : 'This section allows you to manage the general settings of Forumactif Edge.',
+      title : 'إعدادات عامة',
+      fae_gen_desc : 'هذه الفئة تسمح لك بالتعديل على الاعدادات العامة لـForumactif Edge.',
 
-      'fae_label-width' : 'Forum width : ',
-      'fae_gen_tip-width' : 'Drag the slider to adjust the width of the forum.',
+      'fae_label-width' : 'عرض المنتدى :',
+      'fae_gen_tip-width' : 'الرجاء الاستعانة بالمؤشر لضبط عرض منتداك.',
 
-      'fae_label-navbar' : 'Navbar position : ',
-      'fae_gen_tip-navbar' : 'Position of the navbar links.',
+      'fae_label-navbar' : 'موقع أزرار التصفح :',
+      'fae_gen_tip-navbar' : 'موقع أزرار التصفح أعلى المنتدى.',
 
-      'fae_label-logo' : 'Logo position : ',
-      'fae_gen_tip-logo' : 'Position of the forum logo.',
+      'fae_label-logo' : 'موقع اللوغو :',
+      'fae_gen_tip-logo' : 'موقع العارضة الرئيسية للمنتدى',
 
-      'fae_label-profile' : 'Profile position : ',
-      'fae_gen_tip-profile' : 'Position of the post profile in topics.',
+      'fae_label-profile' : 'البيانات الشخصية :',
+      'fae_gen_tip-profile' : 'موقع البيانات الشخصية للعضو في المساهمات',
 
-      fae_update_general : 'Save Changes',
-      fae_gen_updating : 'Updating general settings..',
-      fae_gen_updated : 'General settings have been updated successfully !'
+      fae_update_general : 'حفظ التغييرات',
+      fae_gen_updating : 'حفظ التغييرات...',
+      fae_gen_updated : 'تم إحداث التغييرات بنجاح!'
     },
 
 
     theme_management : {
-      title : 'Theme Management',
-      fae_theme_desc : 'This section allows you to manage the default theme and colors for Forumactif Edge by importing a new theme from the Github repository.',
+      title : 'تصميم المنتدى',
+      fae_theme_desc : 'تسمح لك هذه الفئة بإدارة التصميم الافتراضي للمنتدى و ألوانه و ذلك عبر تحميل التصاميم الجديدة من موقع Github.',
 
-      'fae_label-theme' : 'Select a theme : ',
-      'fae_theme_tip-theme' : 'Select the type of theme you want to import.',
+      'fae_label-theme' : 'تصميم المنتدى',
+      'fae_theme_tip-theme' : 'الرجاء إختيار التصميم الذي تريده للمنتدى',
 
-      'fae_label-color' : 'Select a color : ',
-      'fae_theme_tip-color' : 'Select the default color for the chosen theme.',
+      'fae_label-color' : 'إختيار اللون',
+      'fae_theme_tip-color' : 'الرجاء إختيار اللون الافتراضي للمنتدى.',
 
-      'fae_label-minify' : 'Minify Stylesheet : ',
-      'fae_theme_tip-minify' : 'Minified stylesheets take up less space, but aren\'t ideal for editing. If you want to edit the stylesheet of the selected theme, choose "No" instead.',
+      'fae_label-minify' : 'ورقة التصاميم المصغّرة:',
+      'fae_theme_tip-minify' : 'ورقة التصاميم المصغّرة تقلّص من حجم الاكواد الانسيابية، لكنها ليس الاختيار الامثل للتعديل. إذا أردت التعديل على الاكواد الانسيابية، الرجاء اختيار "لا"',
 
-      'fae_label-rtl' : 'Right-to-Left : ',
-      'fae_theme_tip-rtl' : 'Changes the theme\'s text direction from left-to-right (ltr) to right-to-left (rtl) for rtl scripts.',
+      'fae_label-rtl' : 'الكتابة من اليمين إلى اليسار RTL :',
+      'fae_theme_tip-rtl' : 'تغيير التصميم إلى تصميم متوجّه من اليمين إلى الشمال و ذلك للغات ذات هذا الاتجاه (مثل العربية)',
 
-      fae_import_theme : 'Import theme',
-      fae_import_confirm : 'Are you sure you want to import the theme "{THEME_NAME}" ?\\\n\\\nPlease make sure to back up your current stylesheet if you want to keep it, because it will be overwritten when this new theme is imported. Choose "Cancel" if you\'re not ready to import a new theme.'
+      fae_import_theme : 'تحميل التصميم',
+      fae_import_confirm : 'هل أنت متأكد من رغبتك تحميل التصميم "{THEME_NAME}" ?الرجاء التأكد من احتفاضك بنسخة احتياطية من ورقة الاكواد الانسيابية في حال رغبتك العودة إليها، لأنه سيتم تغييرها عند تطبيق هذا التصميم. الرجاء الضغط على "إلغاء" في حالة عدم استعدادك التام.'
     }
   }
 

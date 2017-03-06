8599154ee188dc5e6c5536e50f169140acea9cc3
Seth Clydesdale <sethc1995@gmail.com>
2017-03-06 14:06:43 -0500

    added viewcomments_body

--- updates/1.1.6.js
+++ updates/1.1.6.js
@@ -4,7 +4,7 @@
 
   - Updated Font Awesome from 4.6.3 to 4.7.0
   - Fixed and updated Addthis share module in the topics
-  - Fixed share button margin and alignment in the topics
+  - Fixed share button margin and alignment above the topics
   - Fixed who is online icon alignment
 
 */
@@ -77,5 +77,42 @@ FAE.update_step = [
     info : 'Publishing template viewtopic_body.html',
     type : 'PUBLISH',
      tpl : 127
+  },
+
+
+  {
+    info : 'Getting template viewcomments_body.html',
+    type : 'GET',
+     url : '/admin/index.forum?part=themes&sub=templates&mode=edit_main&t=131&l=main&extended_admin=1&tid=' + FAE.tid,
+    func : function(d) {
+      var form = $('form[name="post"]', d)[0];
+
+      if (form) {
+        FAE.step[FAE.index + 1].data.template = form.template.value
+                                                .replace("    _atc.cwait = 0;\n    $('.addthis_button').mouseup(function(){\n        if ($('#at15s').css('display') == 'block') {\n            addthis_close();\n        }\n    });", '')
+                                                .replace('http://www.addthis.com/bookmark.php?v=250&amp;pub=forumotion', '#')
+                                                .replace('{JS_DIR}addthis/addthis_widget.js', '//s7.addthis.com/js/300/addthis_widget.js#pubid=forumotion');
+      }
+    }
+  },
+
+
+  {
+    info : 'Updating template viewcomments_body.html',
+    type : 'POST',
+     url : 'part=themes&sub=templates&mode=edit_main&extended_admin=1',
+    data : {
+             t : 131,
+             l : 'main',
+      tpl_name : 'viewcomments_body',
+        submit : 'Save'
+    }
+  },
+
+
+  {
+    info : 'Publishing template viewcomments_body.html',
+    type : 'PUBLISH',
+     tpl : 131
   }
 ];

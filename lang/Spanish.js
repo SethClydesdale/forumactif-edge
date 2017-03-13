d973ea2b4c2a6b0a3a6c82224e987fbdab08a9f3
Seth Clydesdale <sethc1995@gmail.com>
2017-03-13 13:23:14 -0400

    Create Spanish.js

---
+++ lang/Spanish.js
@@ -0,0 +1,228 @@
+// Spanish Translation
+// Translation by Laura (http://fmdesign.forumotion.com/t1287-forumactif-edge-spanish-translation#29673)
+FAE.lang = {
+
+  language : 'Español',
+
+  css : {
+    offline : 'FUERA DE LÍNEA',
+    online : 'EN LÍNEA',
+    total_posts : 'Mensajes totales',
+    total_topics : 'Temas totales',
+    total_users : 'Miembros totales ',
+    record_users : 'Mayor cantidad de usuarios conectados',
+    board_age : 'Edad del foro (en días)',
+    newest_member : 'Último usuario registrado',
+    hidebox : 'Oculto:',
+    chatbox_tip : 'Pasa el ratón por encima para ver quién está conectado.',
+    chatbox_no_away : 'Ningún usuario está ausente ahora mismo.',
+    chatbox_no_online : 'Ningún usuario está conectado ahora mismo.'
+  },
+
+
+  images : {
+
+    buttons : {
+      i_post : 'https://i58.servimg.com/u/f58/19/06/98/92/new-to10.png?color=primary&size=m',
+      i_reply : 'https://i58.servimg.com/u/f58/19/06/98/92/post-r10.png?color=primary&size=m',
+      i_reply_locked : 'https://i58.servimg.com/u/f58/19/06/98/92/locked10.png?color=secondary&size=m',
+      i_icon_quote : 'https://i58.servimg.com/u/f58/19/06/98/92/quote-10.png?color=primary&size=s',
+      i_icon_multiquote_off : 'https://i58.servimg.com/u/f58/19/06/98/92/multi-10.png?color=primary&size=s',
+      i_icon_multiquote_on : 'https://i58.servimg.com/u/f58/19/06/98/92/multi-10.png?color=secondary&size=s',
+      i_icon_thanks_off : 'https://i58.servimg.com/u/f58/19/06/98/92/thanks10.png?color=primary&size=s',
+      i_icon_thanks_on : 'https://i58.servimg.com/u/f58/19/06/98/92/thanks10.png?color=secondary&size=s',
+      i_icon_edit : 'https://i58.servimg.com/u/f58/19/06/98/92/edit-e10.png?color=primary&size=s',
+      i_icon_delete : 'http://i86.servimg.com/u/f86/18/21/41/30/delete10.png?color=primary&size=s',
+      i_icon_ip : 'http://i86.servimg.com/u/f86/18/21/41/30/ip-f10.png?color=primary&size=s',
+      i_icon_report : 'http://i86.servimg.com/u/f86/18/21/41/30/report10.png?color=primary&size=s',
+      i_icon_report_new : 'http://i86.servimg.com/u/f86/18/21/41/30/report10.png?color=secondary&size=s',
+      i_icon_report_locked : 'http://i86.servimg.com/u/f86/18/21/41/30/report12.png?color=secondary&size=s',
+      i_icon_lock_report : 'http://i86.servimg.com/u/f86/18/21/41/30/report12.png?color=primary&size=s',
+      i_icon_unlock_report : 'http://i86.servimg.com/u/f86/18/21/41/30/report13.png?color=primary&size=s',
+      i_icon_search : FAE.eGIF,
+      i_icon_profile : 'http://i86.servimg.com/u/f86/18/21/41/30/profil12.png?color=primary&size=s',
+      i_icon_www : 'http://i86.servimg.com/u/f86/18/21/41/30/websit10.png?color=primary&size=s',
+      i_icon_email : 'http://i86.servimg.com/u/f86/18/21/41/30/email-10.png?color=primary&size=s',
+      i_icon_pm : 'http://i86.servimg.com/u/f86/18/21/41/30/pm-f11.png?color=primary&size=s',
+      i_icon_fb : 'http://i86.servimg.com/u/f86/18/21/41/30/facebo11.png?color=primary&size=s',
+      i_icon_twitter : 'http://i86.servimg.com/u/f86/18/21/41/30/twitte10.png?color=primary&size=s',
+      i_icon_pinterest : 'http://i86.servimg.com/u/f86/18/21/41/30/pinter10.png?color=primary&size=s',
+      i_icon_aim : 'http://i86.servimg.com/u/f86/18/21/41/30/aim-f10.png?color=primary&size=s',
+      i_icon_icq_add : 'http://i86.servimg.com/u/f86/18/21/41/30/icq-f10.png?color=primary&size=s',
+      i_icon_msnm : 'http://i86.servimg.com/u/f86/18/21/41/30/msn-f10.png?color=primary&size=s',
+      i_icon_yim : 'http://i86.servimg.com/u/f86/18/21/41/30/yahoo-10.png?color=primary&size=s',
+      i_icon_skype : 'http://i86.servimg.com/u/f86/18/21/41/30/skype-10.png?color=primary&size=s',
+      i_icon_online : FAE.eGIF,
+      i_msg_newpost : 'https://i58.servimg.com/u/f58/19/06/98/92/new-pm10.png?color=primary&size=m',
+      i_msg_inbox : FAE.eGIF,
+      i_msg_sentbox : FAE.eGIF,
+      i_msg_outbox : FAE.eGIF,
+      i_msg_savebox : FAE.eGIF,
+      i_icon_ajax_edit : 'http://i86.servimg.com/u/f86/18/21/41/30/edit-f10.png',
+      i_icon_ajax_valid : 'http://i86.servimg.com/u/f86/18/21/41/30/valid-10.png',
+      i_icon_attachment_see : 'http://i86.servimg.com/u/f86/18/21/41/30/show-a10.png',
+      i_icon_attachment_download : 'http://i86.servimg.com/u/f86/18/21/41/30/downlo10.png',
+
+      // DO NOT TRANSLATE
+      page : 'buttons',
+      submit : 'Save'
+    },
+
+    gallery : {
+      upload_pic : 'https://i58.servimg.com/u/f58/19/06/98/92/upload10.png?color=primary&size=m'
+    }
+
+  },
+
+
+  javascripts : {
+
+    '[FA EDGE] ALL.JS' : {
+      widget_menu_title : '<div class="title module_column_title">Menú de widgets</div>',
+      widget_menu_tooltip : "menu.title = 'Abre/Cierra el menú de widgets';",
+
+      select_theme : 'Selecciona un estilo',
+      random_theme : 'Estilo aleatorio',
+      default_theme : 'Estilo por defecto',
+
+      toolbar_search_placeholder : 'Buscar...',
+
+      quick_nav_title : "title : 'Navegación rápida'",
+      quick_nav_tooltip : "tooltip : 'Abre/Cierra el menú de navegación rápida'",
+      quick_nav_notif : 'Notificaciones',
+      tool_copy_url_tooltip : 'Copia la URL del BBCode',
+      tool_copy_url_prompt : 'Copia la URL del BBCode abajo (CTRL+C)',
+      tool_admin_panel : 'Panel de administración',
+      tool_to_top : 'Parte superior de la página',
+      tool_to_bottom : 'Parte inferior de la página',
+
+      img_resizer_full_size : 'Mostrar en tamaño completo',
+      img_resizer_enlarge : 'Aumentar imagen',
+      img_resizer_reduce : 'Reducir imagen',
+      img_resizer_download : 'Descargar imagen',
+      img_resizer_tooltip : 'Haz click para ver la imagen completa',
+
+      fae_update_found : 'Hay una actualización disponible para Forumactif Edge. Cuando quieras actualizar, por favor dirígete al panel de control de FAE y haz click en "Buscar actualizaciones" para descargarla e instalarla.'
+    },
+
+
+    '[FA EDGE] TOPICS.JS' : {
+      thanked_message : '<div class="thanked-message"><i class="fa fa-thumbs-up"></i> ¡El creador del tema le da las gracias a {USERNAME}! </div>'
+    }
+
+  },
+
+
+  templates : {
+    'overall_header.html' : {
+      announcements : '<div id="fa_ticker_title" class="title">Anuncios</div>'
+    },
+
+
+    'overall_footer_end.html' : {
+      back_to_top : 'Vuelve arriba',
+      fae_feedback : 'Tu opinión sobre Forumactif Edge',
+      fae_feedback_tooltip : 'Ayúdanos a mejorar Forumactif Edge'
+    },
+
+
+    logged_out_reply : {
+      main_title : '<h2 class="lor_maintitle">Regístrate o conéctate para responder</h2>',
+      main_desc : '<p class="lor_maindesc">Necesitas ser un usuario registrado para poder responder.</p>',
+
+      create_account_title : '<h3 class="lor_subtitle">Crear una cuenta.</h3>',
+      create_account_desc : '<p class="lor_subdesc">Únete a nuestra comunidad creando una nueva cuenta. ¡Es fácil!</p>',
+      create_account_button : '<a href="/register" class="button1">Crear una nueva cuenta</a>',
+
+      log_in_title : '<h3 class="lor_subtitle">Conectarse</h3>',
+      log_in_desc : '<p class="lor_subdesc">¿Ya tienes una cuenta? No hay problema, conéctate aquí.</p>',
+      log_in_button : '<a href="/login" class="button1">Conectarse</a>'
+    }
+
+  },
+
+
+  fae_cp : {
+    fae_cp_main_title : 'Panel de control de Forumactif Edge',
+    fae_cp_desc : 'Este panel te permite instalar, desinstalar y actualizar <a href="https://github.com/SethClydesdale/forumactif-edge" target="_blank">Forumactif Edge</a>. ¡Prepárate para experimentar tu foro de una forma más moderna y adaptable!',
+    fae_log : 'Registro',
+    fae_actions : 'Acciones',
+
+    fae_install : 'Instalar',
+    fae_reinstall : 'Reinstalar',
+    fae_install_complete : '¡COMPLETADO!',
+    fae_install_warning : '¿Seguro que quieres {INSTALL} Forumactif Edge? Esto sobrescribirá tu tema actual y hará que se eliminen todos tus estilos CSS y Javascripts.\\\n\\\nPor favor, asegúrate de hacer copia de seguridad de todos tus archivos personales de contenido, como estilos CSS, templates, y Javascripts antes de continuar. Haz click en "Cancelar" si aún no quieres instalar Forumactif Edge.',
+
+    fae_uninstall : 'Desinstalar',
+    fae_uninstall_warning : '¿Seguro que quieres desinstalar Forumactif Edge? Todos los estilos CSS, Javascripts, y cambios en las templates se borrarán. El foro adoptará el estilo por defecto de la versión phpbb3. \\\n\\\nPor favor, asegúrate de hacer copia de seguridad de todos tus archivos personales de contenido, como estilos CSS, templates, y Javascripts antes de continuar. Haz click en "Cancelar" si aún no quieres desinstalar Forumactif Edge.',
+
+    fae_update : 'Buscar actualizaciones',
+    fae_update_good : '¡Forumactif Edge está actualizado!',
+    fae_update_check : 'Buscando actualizaciones en Github...',
+    fae_update_prepare : 'Preparándose para recuperar instrucciones de la actualización, no cierres esta pestaña, por favor...',
+    fae_update_start : 'La actualización está a punto de comenzar, por favor, no cierres esta pestaña.',
+    fae_update_fetch : 'Recuperando las instrucciones para la actualización de la versión {VERSION_STRING}... ({UPDATE_MIN}/{UPDATE_MAX})',
+    fae_update_notes : 'Por favor <a href="{RELEASE_TAG}" target="_blank">haz click aquí</a> para ver los cambios realizados en la versión {VERSION_STRING}.',
+    fae_update_error : 'Las instrucciones de la actualización para la versión {VERSION_STRING} no se han podido encontrar. Por favor, <a href="http://fmdesign.forumotion.com/t700-forumactif-edge-support#13923" target="_blank">abre un nuevo ticket de soporte</a> y aporta esta información para obtener asistencia.',
+
+    fae_translate : 'Cambiar idioma.',
+    fae_translate_same : 'Forumactif Edge ya está en {LANG}. Por favor, escoge otro idioma.',
+    fae_translate_warning : '¿Seguro que quieres cambiar el idioma de Forumactif Edge a {LANG} ?',
+    fae_translate_start : 'La traducción de Forumactif Edge empezará en breve. Por favor, espera...',
+    fae_translate_get : 'Obteniendo datos del lenguaje {LANG}...',
+    fae_translate_loaded : 'Se han cargado los datos del lenguaje. El proceso de traducción empezará ahora, por favor, no cierres esta pestaña.',
+
+    fae_err_not_founder : 'Sólo <a href="/u1">el fundador</a> puede usar este panel de control. Por favor, ponte en contacto con esa persona para instalar Forumactif Edge. ',
+
+    left : 'Izquierda',
+    center : 'Centro',
+    right : 'Derecha',
+    yes : 'Sí',
+    no : 'No',
+    help : '¡Ayuda!',
+
+    general_settings : {
+      title : 'Opciones generales',
+      fae_gen_desc : 'Esta sección te permite gestionar las opciones generales de Forumactif Edge.',
+
+      'fae_label-width' : 'Ancho del foro :',
+      'fae_gen_tip-width' : 'Arrastra la barra para ajustar el ancho del foro.',
+
+      'fae_label-navbar' : 'Posición de la barra de navegación :',
+      'fae_gen_tip-navbar' : 'Posición de los enlaces de la barra de navegación.',
+
+      'fae_label-logo' : 'Posición del banner :',
+      'fae_gen_tip-logo' : 'Posición del banner del foro.',
+
+      'fae_label-profile' : 'Posición del perfil :',
+      'fae_gen_tip-profile' : 'Posición del perfil en los mensajes de los temas. ',
+
+      fae_update_general : 'Guardar cambios',
+      fae_gen_updating : 'Actualizando opciones generales...',
+      fae_gen_updated : '¡La configuración de las opciones generales se ha actualizado con éxito!'
+    },
+
+
+    theme_management : {
+      title : 'Gestión de los estilos',
+      fae_theme_desc : 'Esta sección te permite gestionar el estilo y los colores por defecto para Foroactif Edge mediante la importación de un estilo de la carpeta de Github.',
+
+      'fae_label-theme' : 'Seleccionar un estilo :',
+      'fae_theme_tip-theme' : 'Selecciona el tipo de estilo que quieres importar.',
+
+      'fae_label-color' : 'Selecciona un color :',
+      'fae_theme_tip-color' : 'Selecciona el color por defecto para el estilo escogido. ',
+
+      'fae_label-minify' : 'Hoja de estilos reducida :',
+      'fae_theme_tip-minify' : 'Las hojas de estilos reducidas ocupan menos espacio, pero son más complicadas de editar. Si quieres editar los estilos CSS del estilo seleccionado, escoge "no". ',
+
+      'fae_label-rtl' : 'Derecha a izquierda :',
+      'fae_theme_tip-rtl' : 'Cambia la dirección del estilo de izquierda a derecha (ltr) a derecha a izquierda (rtl) para scripts rtl. ',
+
+      fae_import_theme : 'Importar estilo',
+      fae_import_confirm : '¿Seguro que quieres importar el estilo "{THEME_NAME}" ?\\\n\\\nPor favor, asegúrate de hacer copia de seguridad de tu hoja de estilos CSS actual si quieres mantenerlos, porque se sobrescribirá cuando se importe este nuevo estilo. Escoge "Cancelar" si no quieres importar un estilo nuevo aún. '
+    }
+
+  }
+
+};

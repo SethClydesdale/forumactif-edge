0088d94e5c4aca6105a4dfb1f6a230a419661f62
Seth Clydesdale <sethc1995@gmail.com>
2017-03-15 20:09:40 -0400

    Update javascript.js

--- docs/javascript.js
+++ docs/javascript.js
@@ -1,3 +1,59 @@
 (function() {
 
+  /* -- DEMO VIDEOS -- */
+  for (var a = document.querySelectorAll('.bubble[data-video]'), i = 0, j = a.length; i < j; i++) {
+
+    // adds event listener to the bubbles, so they display previews on click
+    a[i].title = 'View Demo';
+    a[i].addEventListener('click', function () {
+      var overlay = document.createElement('DIV')
+            video = document.createElement('VIDEO'),
+             frag = document.createDocumentFragment();
+
+      if (/giveTip/.test(this.className)) {
+        this.className = this.className.replace('giveTip', '');
+      }
+
+      overlay.id = 'previewOverlay';
+      overlay.addEventListener('click', closePreview);
+
+      video.id = 'previewVideo';
+      video.width = window.innerWidth / 1.1;
+      video.height = window.innerWidth / 2;
+      video.style.marginLeft = '-' + ((video.width / 2) + 12) + 'px';
+      video.style.marginTop = '-' + ((video.height / 2) + 3) + 'px';
+      video.innerHTML = '<source src="video/' + this.dataset.video + '" type="video/mp4">Your browser does not support the video tag.';
+      video.addEventListener('ended', closePreview);
+
+      frag.appendChild(overlay);
+      frag.appendChild(video);
+      document.body.appendChild(frag);
+      document.body.style.overflow = 'hidden';
+
+      window.setTimeout(function () {
+        overlay.className = 'visible';
+        video.className = 'visible';
+        video.play();
+      }, 100);
+    });
+
+  }
+
+  // closes preview when overlay is clicked or the video ends
+  function closePreview () {
+    if (this.className == 'visible') {
+      var overlay = document.getElementById('previewOverlay'),
+            video = document.getElementById('previewVideo');
+
+      overlay.className = '';
+      video.className = '';
+
+      window.setTimeout(function () {
+        document.body.removeChild(overlay);
+        document.body.removeChild(video);
+        document.body.style.overflow = '';
+      }, 400);
+    }
+  };
+
 }());

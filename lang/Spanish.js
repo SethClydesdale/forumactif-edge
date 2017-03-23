0c6ae2d6630ab15bc305afbc2643fbe8b2b772db
Seth Clydesdale <sethc1995@gmail.com>
2017-03-23 13:02:03 -0400

    add v1.2.0 translation

--- lang/Spanish.js
+++ lang/Spanish.js
@@ -82,8 +82,15 @@ FAE.lang = {
       widget_menu_tooltip : "menu.title = 'Abre/Cierra el menú de widgets';",
 
       select_theme : 'Selecciona un estilo',
+      custom_theme : 'Estilo personalizado',
       random_theme : 'Estilo aleatorio',
       default_theme : 'Estilo por defecto',
+
+      dark_mode : 'Modo oscuro :',
+      light_mode : 'Modo claro :',
+
+      code_copy : 'Copiar código',
+      code_copied : '¡Copiado!',
 
       toolbar_search_placeholder : 'Buscar...',
 
@@ -171,8 +178,23 @@ FAE.lang = {
     fae_translate_start : 'La traducción de Forumactif Edge empezará en breve. Por favor, espera...',
     fae_translate_get : 'Obteniendo datos del lenguaje {LANG}...',
     fae_translate_loaded : 'Se han cargado los datos del lenguaje. El proceso de traducción empezará ahora, por favor, no cierres esta pestaña.',
+    fae_translate_submit : 'Enviar',
+    fae_translate_submit_option : 'Enviar una nueva traducción',
+    fae_translate_submit_confirm : 'Has escogido enviar una nueva traducción. Si esto es correcto, por favor, selecciona "OK" y ve a la página de traducción. En caso contrario, haz click en "cancelar" y escoge otro idioma.',
 
-    fae_err_not_founder : 'Sólo <a href="/u1">el fundador</a> puede usar este panel de control. Por favor, ponte en contacto con esa persona para instalar Forumactif Edge. ',
+    configuration_title : 'Configuración',
+    configuration_desc : 'Hacer click en los botones de abajo te llevará a distintas secciones del panel de control, que te permiten gestionar la configuración de Forumactif Edge. Adelante, explora cada una de esas secciones para empezar a personalizar el estilo de tu foro. Si necesitas más información, <a href="https://github.com/SethClydesdale/forumactif-edge/wiki/FAE-Control-Panel-Guide" target="_blank">haz click aquí</a> para ver la guía del panel de control.',
+
+    fae_err_not_founder : 'Sólo <a href="/u1">el fundador</a> puede acceder a la opción de instalar. Por favor, ponte en contacto con esa persona para que te ayude a instalar Forumactif Edge.',
+    co_admin : 'Sólo <a href="/u1">el fundador</a> puede tener acceso a las opciones de instalar, actualizar, y traducir. Como Forumactif Edge está instalado, puedes acceder a algunas características principales para personalizar tu foro mediante la sección "Configuración", abajo.',
+    reload_page : 'Por favor, <a href="javascript:window.location.reload();">haz click aquí</a> para volver a cargar la página.',
+
+
+    welcome_message : '¡Bienvenido al panel de control de FAE! El panel de control de FAE te permitirá instalar, actualizar, traducir, y personalizar Forumactif Edge. Para aprender más sobre cómo usar el panel de control, se recomienda que leas la <a href="https://github.com/SethClydesdale/forumactif-edge/wiki/FAE-Control-Panel-Guide" target="_blank">Guía del panel de control</a> en la wiki de GitHub.',
+    auto_version_local : 'La versión de Forumactif Edge que tienes instalada es <a href="https://github.com/SethClydesdale/forumactif-edge/releases/tag/{VERSION_STRING}" target="_blank">{VERSION_STRING}</a>.',
+    auto_version_github : 'La versión actual de Forumactif Edge en GitHub es <a href="https://github.com/SethClydesdale/forumactif-edge/releases/tag/{VERSION_STRING}" target="_blank">{VERSION_STRING}</a>.',
+    update_waiting : 'Hay actualizaciones disponibles para Forumactif Edge. Échale un vistazo a <a href="https://github.com/SethClydesdale/forumactif-edge/wiki/Checking-for-updates" target="_blank">la wiki</a> para más información sobre actualizar.',
+    release_list : '<a href="https://github.com/SethClydesdale/forumactif-edge/releases" target="_blank">Haz click aquí</a> para ver la lista completa de versiones lanzadas para Forumactif Edge.',
 
     left : 'Izquierda',
     center : 'Centro',
@@ -203,6 +225,53 @@ FAE.lang = {
     },
 
 
+    colors : {
+      title : 'Colores',
+      fae_colors_desc : 'Esta sección te permite cambiar los colores del estilo de tu foro. Selecciona un color abajo y previsualiza el resultado en la ventana de la derecha para empezar. Cuando hayas acabado, haz click en "Actualizar colores" para aplicar tus nuevos colores al foro.',
+      'fae_colors_title-preview' : 'Previsualización ',
+
+      'fae_colors_title-primary' : 'Colores principales',
+      'fae_colors_desc-primary' : 'Las opciones de abajo te permiten ajustar los 5 tonos principales para Forumactif Edge.',
+      'fae_label-color-p1' : 'Color principal 1 :',
+      'fae_label-color-p2' : 'Color principal 2 :',
+      'fae_label-color-p3' : 'Color principal 3 :',
+      'fae_label-color-p4' : 'Color principal 4 :',
+      'fae_label-color-p5' : 'Color principal 5 :',
+
+      'fae_colors_title-general' : 'Colores generales',
+      'fae_colors_desc-general' : 'Las opciones de abajo te permiten cambiar colores varios del foro. ',
+      'fae_label-color-bg1' : 'Color principal de fondo 1 :',
+      'fae_label-color-bg2' : 'Color principal de fondo 2 :',
+      'fae_label-color-m1' : 'Color principal de los bordes :',
+      'fae_label-color-bg3' : 'Color de la barra de votos / casilla de verificación :',
+      'fae_label-color-bg4' : 'Color de foro leído / desconectado :',
+      'fae_label-color-bg5' : 'Color de conectado / gracias :',
+      'fae_label-color-bg6' : 'Color al pasar el ratón por encima de las filas :',
+      'fae_label-color-m2' : 'Color de fondo de la barra de desplazamiento :',
+
+      'fae_colors_title-font' : 'Ajustes de la fuente',
+      'fae_colors_desc-font' : 'Las siguientes opciones te permiten cambiar el tamaño, medida, y estilo de la fuente del foro.',
+      'fae_label-color-f0' : 'Color del texto :',
+      'fae_label-color-f5' : 'Color de los enlaces :',
+      'fae_label-color-f6' : 'Color de los enlaces visitados :',
+      'fae_label-color-f7' : 'Color de los enlaces al pasar el ratón por encima :',
+      'fae_label-color-f8' : 'Color de los enlaces activos :',
+      'fae_label-color-f1' : 'Tamaño de la fuente principal :',
+      'fae_label-color-f2' : 'Estilo de la fuente principal :',
+      'fae_label-color-f3' : 'Estilo de la fuente de los títulos :',
+      'fae_label-color-f4' : 'Estilo de la fuente para código :',
+
+      fae_save_colors : 'Actualizar colores',
+      fae_default_colors : 'Volver a los colores predeterminados',
+
+      confirm_update : '¿Seguro que quieres borrar tu configuración de colores personalizada y volver a utilizar los colores por defecto?',
+      updating : 'Actualizando los colores del foro...',
+      updated : '¡Los colores del foro se han actualizado con éxito!',
+      reverting : 'Volviendo a la configuración de colores predeterminada...',
+      reverted : '¡Los colores del foro se han cambiado a los ajustes por defecto!'
+    },
+
+
     theme_management : {
       title : 'Gestión de los estilos',
       fae_theme_desc : 'Esta sección te permite gestionar el estilo y los colores por defecto para Foroactif Edge mediante la importación de un estilo de la carpeta de Github.',
@@ -221,6 +290,34 @@ FAE.lang = {
 
       fae_import_theme : 'Importar estilo',
       fae_import_confirm : '¿Seguro que quieres importar el estilo "{THEME_NAME}" ?\\\n\\\nPor favor, asegúrate de hacer copia de seguridad de tu hoja de estilos CSS actual si quieres mantenerlos, porque se sobrescribirá cuando se importe este nuevo estilo. Escoge "Cancelar" si no quieres importar un estilo nuevo aún. '
+    },
+
+
+    plugin_management : {
+      title : 'Gestión de extensiones',
+      fae_plugin_desc : 'Esta sección te permite configurar las extensiones básicas de Forumactif Edge.',
+
+      'fae_label-qnp' : 'Posición de la navegación rápida :',
+      'fae_plugin_tip-qnp' : 'Posición del menú lateral de navegación rápida.',
+
+      'fae_label-qns' : 'Mostrar siempre la navegación rápida :',
+      'fae_plugin_tip-qns' : 'Por defecto la navegación rápida sólo se muestra cuando la barra de navegación no está visible en la pantalla. Habilitar esta opción permitirá que la navegación rápida siempre sea visible.',
+
+      'fae_label-tso' : 'Opciones del selector de estilos :',
+      'fae_plugin_tip-tso' : 'Esta herramienta te permite editar la lista del selector de estilos, y te da la posibilidad de añadir, borrar, y editar estilos.',
+
+      fae_themer_add : 'Nuevo estilo',
+      fae_themer_import : 'Importar predeterminados',
+      fae_import_confirm : '¿Quieres importar la lista de estilos predeterminados desde GitHub?',
+      fae_importing : 'Contactando con GitHub, espera, por favor...',
+      fae_compiling : 'Compilando estilos, espera, por favor...',
+
+      fae_save_plugins : 'Actualizar extensiones',
+      fae_finding_plugins : 'Localizando [FA EDGE] ALL.JS...',
+      fae_plugins_found : '¡Encontrado [FA EDGE] ALL.JS!',
+      fae_getting_plugins : 'Obteniendo [FA EDGE] ALL.JS...',
+      fae_updating_plugins : 'Actualizando tus extensiones...',
+      fae_plugins_updated : '¡Las extensiones se han actualizado con éxito!'
     }
 
   }

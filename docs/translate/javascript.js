b5a8240dca8da95edae5696fc1e8529c92e14c48
Seth Clydesdale <sethc1995@gmail.com>
2017-03-23 20:44:03 -0400

    save scrollPosition

--- docs/translate/javascript.js
+++ docs/translate/javascript.js
@@ -1,6 +1,8 @@
 // globals
 var textarea = document.getElementById('webpage-code'),
-    preview = document.getElementById('webpage-preview'),
+    frame = document.getElementById('webpage-preview'),
+    preview,
+    scrollPosition = 0,
     GET = new XMLHttpRequest(),
     original = ''; // used to store original webpage code
 
@@ -17,7 +19,7 @@ function initTranslator (string) {
                    toTranslate[i].tagName == 'META' ? toTranslate[i].outerHTML.replace(/.*?content="(.*?)".*/, '$1') :
                    toTranslate[i].outerHTML.replace(/^<.*?>/, '').replace(/<\/[^>]*?>$/, '');
       text.dataset.alias = (toTranslate[i].dataset.tip || toTranslate[i].tagName == 'META') ? text.value : toTranslate[i].outerHTML;
-      text.onkeyup = function () {
+      text.onkeydown = function () {
         // get all translations
         for (var a = document.querySelectorAll('.translation'), i = 0, j = a.length, replacement = original, openTag, endTag; i < j; i++) {
           try {
@@ -33,10 +35,7 @@ function initTranslator (string) {
 
         textarea.value = replacement; // update the webpage code
 
-        // update the preview
-        preview.open();
-        preview.write(textarea.value);
-        preview.close();
+        updatePreview(textarea.value);
       }
 
       row = document.createElement('DIV');
@@ -52,17 +51,30 @@ function initTranslator (string) {
   }
 
   // open the iframe and apply the webpage code
-  preview.open();
-  preview.write(textarea.value);
-  preview.close();
+  updatePreview(textarea.value);
 
   // wait until the frame contents are loaded, and then compole our translations by targeting elements we want to translate
   preview.addEventListener('DOMContentLoaded', setup);
+};
 
-}
+
+// open the iframe and apply the webpage code, as well as restore the scroll position
+function updatePreview (value) {
+  preview.open();
+  preview.write(value);
+  preview.close();
+
+  frame.contentWindow.scrollTo(0, scrollPosition); // restore scroll position
+
+  // listen to iframe scroll to keep scroll position while rewriting the document
+  frame.contentWindow.onscroll = function () {
+    scrollPosition = preview.body.scrollTop || preview.documentElement.scrollTop;
+  };
+};
+
 
 try {
-  preview = preview.contentDocument || preview.contentWindow.document;
+  preview = frame.contentDocument || frame.contentWindow.document;
 
   GET.onreadystatechange = function () {
     if (this.readyState == 4 && this.status == 200) {
@@ -78,6 +90,7 @@ try {
     initTranslator(textarea.value);
   };
 
+  // warn before leaving page, in case changes haven't been saved
   window.onbeforeunload = function (e) {
     var warning = 'Are you sure you want to leave ? Any changes you have made will be lost.';
     e.returnValue = warning;

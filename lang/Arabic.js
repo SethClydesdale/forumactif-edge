d28ed256ae1d5a57beb6ce45d63d69745159a473
Seth Clydesdale <sethc1995@gmail.com>
2017-03-24 16:02:34 -0400

    Update Arabic.js

--- lang/Arabic.js
+++ lang/Arabic.js
@@ -82,8 +82,15 @@ FAE.lang = {
       widget_menu_tooltip : "menu.title = 'قائمة جانبية';",
 
       select_theme : 'الرجاء إختيار لون',
+      custom_theme : 'Custom theme',
       random_theme : 'لون عشوائي',
       default_theme : 'اللون الافتراضي',
+
+      dark_mode : 'Dark Mode : ',
+      light_mode : 'Light Mode : ',
+
+      code_copy : 'Copy Code',
+      code_copied : 'Copied !',
 
       toolbar_search_placeholder : 'بحث...',
 

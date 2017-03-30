1b35a1f3dc47b6845761afea9f35a2f7fe7a54a9
Seth Clydesdale <sethc1995@gmail.com>
2017-03-30 15:36:59 -0400

    Create Russian.js

---
+++ lang/Russian.js
@@ -0,0 +1,325 @@
+// Russian Translation
+// Translation by Ivan (http://fmdesign.forumotion.com/t1301-forumactif-edge-russian-translation#29837)
+FAE.lang = {
+
+  language : 'Русский',
+
+  css : {
+    offline : 'Не в сети',
+    online : 'В сети',
+    total_posts : 'Всего сообщений',
+    total_topics : 'Всего тем',
+    total_users : 'Всего пользователей',
+    record_users : 'Максимально пользователей в сети',
+    board_age : 'Возраст форума (в днях)',
+    newest_member : 'Новый пользователь:',
+    hidebox : 'Скрытых:',
+    chatbox_tip : 'Наведите указатель мыши, чтобы узнать, кто в сети.',
+    chatbox_no_away : 'В настоящее время ни одного участника нет.',
+    chatbox_no_online : 'В настоящее время ни одного пользователя нет в сети.'
+  },
+
+
+  images : {
+
+    buttons : {
+      i_post : 'https://i58.servimg.com/u/f58/18/21/41/30/new-to10.png?color=primary&size=m',
+      i_reply : 'https://i58.servimg.com/u/f58/18/21/41/30/post-r10.png?color=primary&size=m',
+      i_reply_locked : 'https://i58.servimg.com/u/f58/18/21/41/30/locked10.png?color=secondary&size=m',
+      i_icon_quote : 'https://i58.servimg.com/u/f58/18/21/41/30/quote-10.png?color=primary&size=s',
+      i_icon_multiquote_off : 'https://i58.servimg.com/u/f58/18/21/41/30/multi-10.png?color=primary&size=s',
+      i_icon_multiquote_on : 'https://i58.servimg.com/u/f58/18/21/41/30/multi-10.png?color=secondary&size=s',
+      i_icon_thanks_off : 'https://i58.servimg.com/u/f58/18/21/41/30/thanks10.png?color=primary&size=s',
+      i_icon_thanks_on : 'https://i58.servimg.com/u/f58/18/21/41/30/thanks10.png?color=secondary&size=s',
+      i_icon_edit : 'https://i58.servimg.com/u/f58/18/21/41/30/edit-r10.png?color=primary&size=s',
+      i_icon_delete : 'http://i86.servimg.com/u/f86/18/21/41/30/delete10.png?color=primary&size=s',
+      i_icon_ip : 'http://i86.servimg.com/u/f86/18/21/41/30/ip-f10.png?color=primary&size=s',
+      i_icon_report : 'http://i86.servimg.com/u/f86/18/21/41/30/report10.png?color=primary&size=s',
+      i_icon_report_new : 'http://i86.servimg.com/u/f86/18/21/41/30/report10.png?color=secondary&size=s',
+      i_icon_report_locked : 'http://i86.servimg.com/u/f86/18/21/41/30/report12.png?color=secondary&size=s',
+      i_icon_lock_report : 'http://i86.servimg.com/u/f86/18/21/41/30/report12.png?color=primary&size=s',
+      i_icon_unlock_report : 'http://i86.servimg.com/u/f86/18/21/41/30/report13.png?color=primary&size=s',
+      i_icon_search : FAE.eGIF,
+      i_icon_profile : 'http://i86.servimg.com/u/f86/18/21/41/30/profil12.png?color=primary&size=s',
+      i_icon_www : 'http://i86.servimg.com/u/f86/18/21/41/30/websit10.png?color=primary&size=s',
+      i_icon_email : 'http://i86.servimg.com/u/f86/18/21/41/30/email-10.png?color=primary&size=s',
+      i_icon_pm : 'http://i86.servimg.com/u/f86/18/21/41/30/pm-f11.png?color=primary&size=s',
+      i_icon_fb : 'http://i86.servimg.com/u/f86/18/21/41/30/facebo11.png?color=primary&size=s',
+      i_icon_twitter : 'http://i86.servimg.com/u/f86/18/21/41/30/twitte10.png?color=primary&size=s',
+      i_icon_pinterest : 'http://i86.servimg.com/u/f86/18/21/41/30/pinter10.png?color=primary&size=s',
+      i_icon_aim : 'http://i86.servimg.com/u/f86/18/21/41/30/aim-f10.png?color=primary&size=s',
+      i_icon_icq_add : 'http://i86.servimg.com/u/f86/18/21/41/30/icq-f10.png?color=primary&size=s',
+      i_icon_msnm : 'http://i86.servimg.com/u/f86/18/21/41/30/msn-f10.png?color=primary&size=s',
+      i_icon_yim : 'http://i86.servimg.com/u/f86/18/21/41/30/yahoo-10.png?color=primary&size=s',
+      i_icon_skype : 'http://i86.servimg.com/u/f86/18/21/41/30/skype-10.png?color=primary&size=s',
+      i_icon_online : FAE.eGIF,
+      i_msg_newpost : 'https://i58.servimg.com/u/f58/18/21/41/30/new-pm10.png?color=primary&size=m',
+      i_msg_inbox : FAE.eGIF,
+      i_msg_sentbox : FAE.eGIF,
+      i_msg_outbox : FAE.eGIF,
+      i_msg_savebox : FAE.eGIF,
+      i_icon_ajax_edit : 'http://i86.servimg.com/u/f86/18/21/41/30/edit-f10.png',
+      i_icon_ajax_valid : 'http://i86.servimg.com/u/f86/18/21/41/30/valid-10.png',
+      i_icon_attachment_see : 'http://i86.servimg.com/u/f86/18/21/41/30/show-a10.png',
+      i_icon_attachment_download : 'http://i86.servimg.com/u/f86/18/21/41/30/downlo10.png',
+
+      // DO NOT TRANSLATE
+      page : 'buttons',
+      submit : 'Save'
+    },
+
+    gallery : {
+      upload_pic : 'https://i58.servimg.com/u/f58/18/21/41/30/upload10.png?color=primary&size=m'
+    }
+
+  },
+
+
+  javascripts : {
+
+    '[FA EDGE] ALL.JS' : {
+      widget_menu_title : '<div class="title module_column_title">Меню виджета</div>',
+      widget_menu_tooltip : "menu.title = 'Переключить меню виджетов';",
+
+      select_theme : 'Выберите тему',
+      custom_theme : 'Своя тема',
+      random_theme : 'Случайная тема',
+      default_theme : 'Стандартная тема',
+
+      dark_mode : 'Темный режим:',
+      light_mode : 'Светлый режим:',
+
+      code_copy : 'Копировать код',
+      code_copied : 'Скопировано!',
+
+      toolbar_search_placeholder : 'Поиск...',
+
+      quick_nav_title : "title : 'Быстрая навигация'",
+      quick_nav_tooltip : "tooltip : 'Включить / выключить быструю навигацию'",
+      quick_nav_notif : 'Уведомления',
+      tool_copy_url_tooltip : 'Скопировать BBCode URL-адрес',
+      tool_copy_url_prompt : 'Скопируйте URL-адрес BBCode ниже (CTRL + C)',
+      tool_admin_panel : 'Панель Администратора',
+      tool_to_top : 'Вверх страницы',
+      tool_to_bottom : 'Вниз страницы',
+
+      img_resizer_full_size : 'Показать полный размер',
+      img_resizer_enlarge : 'Увеличить картинку',
+      img_resizer_reduce : 'Уменьшить картинку',
+      img_resizer_download : 'Скачать картинку',
+      img_resizer_tooltip : 'Нажмите, чтобы посмотреть в полный размер',
+
+      fae_update_found : 'Было найдено обновление для форума. Когда все будет готово, перейдите в Панель управления и нажмите «Проверить наличие обновлений», чтобы начать процесс обновления.'
+    },
+
+
+    '[FA EDGE] TOPICS.JS' : {
+      thanked_message : '<div class="thanked-message"><i class="fa fa-thumbs-up"></i> {USERNAME} была благодарность автору темы!</div>'
+    }
+
+  },
+
+
+  templates : {
+    'overall_header.html' : {
+      announcements : '<div id="fa_ticker_title" class="title">Объявления</div>'
+    },
+
+
+    'overall_footer_end.html' : {
+      back_to_top : 'Вернуться к началу',
+      fae_feedback : 'Отправить отзыв',
+      fae_feedback_tooltip : 'Помочь улучшить Forumactif Edge'
+    },
+
+
+    logged_out_reply : {
+      main_title : '<h2 class="lor_maintitle">Создайте учётную запись или войдите, чтобы отвечать</h2>',
+      main_desc : '<p class="lor_maindesc">Вы должны быть участником, чтобы оставить ответ.</p>',
+
+      create_account_title : '<h3 class="lor_subtitle">Создать аккаунт</h3>',
+      create_account_desc : '<p class="lor_subdesc">Присоединяйтесь к нашему сообществу, создав новую учетную запись. Это просто!</p>',
+      create_account_button : '<a href="/register" class="button1">Создать новый аккаунт</a>',
+
+      log_in_title : '<h3 class="lor_subtitle">Войти</h3>',
+      log_in_desc : '<p class="lor_subdesc">У вас уже есть аккаунт? Нет проблем, войдите в систему.</p>',
+      log_in_button : '<a href="/login" class="button1">Войти</a>'
+    }
+
+  },
+
+
+  fae_cp : {
+    fae_cp_main_title : 'Панель управления Forumactif Edge',
+    fae_cp_desc : 'Эта панель позволяет вам устанавливать, удалять и обновлять движок <a href="https://github.com/SethClydesdale/forumactif-edge" target="_blank">Forumactif Edge</a>. Будьте готовы к тому, что ваш форум станет более современным и отзывчивым!',
+    fae_log : 'Журнал',
+    fae_actions : 'Действия',
+
+    fae_install : 'Установить',
+    fae_reinstall : 'Переустановить',
+    fae_install_complete : 'ЗАВЕРШЕНО!',
+    fae_install_warning : 'Вы уверены, что хотите {INSTALL} Forumactif Edge? Это изменит текущее оформление и удалит Ваши коды JavaScript.\\\nПеред продолжением убедитесь, что Вы выполнили резервное копирование всех кодов, таких как CSS, персональные шаблоны и JavaScript. Нажмите «Отмена», если Вы не готовы к установке Forumactif Edge.',
+
+    fae_uninstall : 'Удалить',
+    fae_uninstall_warning : 'Вы действительно хотите удалить Forumactif Edge? Все изменения CSS, JavaScript и шаблонов будут удалены; форум будет возвращен к движку по умолчанию - phpbb3.\\\nПеред продолжением убедитесь, что Вы выполнили резервное копирование всех кодов, таких как CSS, персональные шаблоны и JavaScript. Нажмите «Отмена», если Вы не хотите удалить Forumactif Edge.',
+
+    fae_update : 'Проверить обновления',
+    fae_update_good : 'Forumactif Edge является актуальным!',
+    fae_update_check : 'Проверка наличия обновлений через Github...',
+    fae_update_prepare : 'При подготовке к выполнению обновлений не закрывайте эту вкладку...',
+    fae_update_start : 'Обновления начнутся, пожалуйста, не закрывайте эту вкладку.',
+    fae_update_fetch : 'Получение обновления для версии {VERSION_STRING}... ({UPDATE_MIN}/{UPDATE_MAX})',
+    fae_update_notes : 'Пожалуйста <a href="{RELEASE_TAG}" target="_blank">нажмите здесь</a> для просмотра изменений, сделанных в версии{VERSION_STRING}.',
+    fae_update_error : 'Не удалось найти обновления для версии {VERSION_STRING}. Пожалуйста <a href="http://fmdesign.forumotion.com/t700-forumactif-edge-support#13923" target="_blank">откройте тикет</a> и предоставьте эту информацию для помощи.',
+
+    fae_translate : 'Сменить язык',
+    fae_translate_same : 'Forumactif Edge уже работает на языке: {LANG}. Пожалуйста, выберите другой язык.',
+    fae_translate_warning : 'Вы уверены, что хотите изменить язык интерфейса форума на {LANG}?',
+    fae_translate_start : 'В ближайшее время начнется перевод Forumactif Edge. Пожалуйста, подождите...',
+    fae_translate_get : 'Получение языковых данных - {LANG}...',
+    fae_translate_loaded : 'Языковые данные загружены. Процесс перевода начнется, пожалуйста, не закрывайте эту вкладку.',
+    fae_translate_submit : 'Отправить',
+    fae_translate_submit_option : 'Отправить новый перевод',
+    fae_translate_submit_confirm : 'Вы решили отправить новый перевод. Если это правильно, нажмите «ОК» и перейдите на страницу перевода, иначе нажмите «отмена» и выберите другой язык.',
+
+    configuration_title : 'Конфигурация',
+    configuration_desc : 'Нажав на кнопки ниже, Вы попадете в различные разделы панели управления, которые позволят Вам управлять конфигурацией системы Forumactif Edge. Идите дальше и изучите каждый из этих разделов, чтобы начать работу с персонализацией своей темы. Если Вам нужна дополнительная информация, <a href="https://github.com/SethClydesdale/forumactif-edge/wiki/FAE-Control-Panel-Guide" target="_blank">нажмите здесь</a>, чтобы просмотреть Панель управления.',
+
+    fae_err_not_founder : 'Только <a href="/u1">создатель форума</a> может получить доступ к установке. Пожалуйста, свяжитесь с ними для получения помощи по установке Forumactif Edge.',
+    co_admin : 'Только <a href="/u1">создатель форума</a> может получить доступ к действиям по установке, обновлению и переводу. Поскольку Forumactif Edge установлен, Вы можете получить доступ к некоторым основным функциям для персонализации вашего форума в разделе «Конфигурация» ниже.',
+    reload_page : 'Пожалуйста <a href="javascript:window.location.reload();">нажмите здесь</a>, чтобы обновить эту страницу.',
+
+
+    welcome_message : 'Добро пожаловать в Панель управления! Панель управления позволит Вам устанавливать, обновлять, переводить и настраивать Forumactif Edge. Чтобы узнать больше об использовании Панели управления, рекомендуется прочитать <a href = "https://github.com/SethClydesdale/forumactif-edge/wiki/FAE-Control-Panel-Guide" target = "_ blank ">Руководство по панели управления</a> в Github.',
+    auto_version_local : 'Установленная версия Forumactif Edge - <a href="https://github.com/SethClydesdale/forumactif-edge/releases/tag/{VERSION_STRING}" target="_blank"> {VERSION_STRING} </a>.',
+    auto_version_github : 'Текущая версия Forumactif Edge в Github - <a href="https://github.com/SethClydesdale/forumactif-edge/releases/tag/{VERSION_STRING}" target="_blank"> {VERSION_STRING} </a>.',
+    update_waiting : 'Доступны обновления для Foruamctif Edge. Ознакомьтесь с <a href="https://github.com/SethClydesdale/forumactif-edge/wiki/Checking-for-updates" target="_blank">инструкцией</a> для получения дополнительной информации об обновлении.',
+    release_list : 'Нажмите <a href="https://github.com/SethClydesdale/forumactif-edge/releases" target="_blank">здесь</a>, чтобы просмотреть полный список версий Forumactif Edge.',
+
+    left : 'Слева',
+    center : 'В центре',
+    right : 'Справа',
+    yes : 'Да',
+    no : 'Нет',
+    help : 'Помощь!',
+
+    general_settings : {
+      title : 'Общие настройки',
+      fae_gen_desc : 'В этом разделе вы можете управлять общими настройками движка Forumactif Edge.',
+
+      'fae_label-width' : 'Ширина форума:',
+      'fae_gen_tip-width' : 'Перетащите ползунок, чтобы настроить ширину форума.',
+
+      'fae_label-navbar' : 'Положение панели навигации:',
+      'fae_gen_tip-navbar' : 'Позиция ссылок навигации.',
+
+      'fae_label-logo' : 'Положение логотипа:',
+      'fae_gen_tip-logo' : 'Расположение логотипа форума.',
+
+      'fae_label-profile' : 'Позиция профиля.',
+      'fae_gen_tip-profile' : 'Положение профиля в сообщениях в темах.',
+
+      fae_update_general : 'Сохранить изменения',
+      fae_gen_updating : 'Обновление общих настроек...',
+      fae_gen_updated : 'Общие настройки успешно обновлены!'
+    },
+
+
+    colors : {
+      title : 'Цвета',
+      fae_colors_desc : 'В этом разделе Вы можете изменить цвета своего оформления форума. Выберите цвет снизу и предпросмотрите результат в окне справа, чтобы начать. Когда Вы закончите, нажмите «Обновить цвета», чтобы применить новые цвета.',
+      'fae_colors_title-preview' : 'Предпросмотр',
+
+      'fae_colors_title-primary' : 'Первичные цвета',
+      'fae_colors_desc-primary' : 'Нижеуказанные параметры позволяют настроить 5 основных цветовых оттенков для Forumactif Edge.',
+      'fae_label-color-p1' : 'Основной цвет 1:',
+      'fae_label-color-p2' : 'Основной цвет 2:',
+      'fae_label-color-p3' : 'Основной цвет 3:',
+      'fae_label-color-p4' : 'Основной цвет 4:',
+      'fae_label-color-p5' : 'Основной цвет 5:',
+
+      'fae_colors_title-general' : 'Общие цвета',
+      'fae_colors_desc-general' : 'Опции ниже позволяют Вам изменять различные цвета форума.',
+      'fae_label-color-bg1' : 'Основной цвет фона 1:',
+      'fae_label-color-bg2' : 'Основной цвет фона 2:',
+      'fae_label-color-m1' : 'Основной цвет границы:',
+      'fae_label-color-bg3' : 'Голосовое управление / Цвет флажка:',
+      'fae_label-color-bg4' : 'Цвет чтения форума / не в сети:',
+      'fae_label-color-bg5' : 'Цвет в сети / спасибо:',
+      'fae_label-color-bg6' : 'Цвет строки при наведении указателя мыши:',
+      'fae_label-color-m2' : 'Цвет полосы прокрутки фона:',
+
+      'fae_colors_title-font' : 'Настройки шрифта',
+      'fae_colors_desc-font' : 'Нижеуказанные параметры позволяют изменить цвет шрифта, размер и пользователей форума.',
+      'fae_label-color-f0' : 'Цвет текста:',
+      'fae_label-color-f5' : 'Цвет ссылки:',
+      'fae_label-color-f6' : 'Цвет посещенной ссылки:',
+      'fae_label-color-f7' : 'Цвет ссылки при наведении:',
+      'fae_label-color-f8' : 'Активный цвет ссылок:',
+      'fae_label-color-f1' : 'Основной размер шрифта:',
+      'fae_label-color-f2' : 'Основной шрифт:',
+      'fae_label-color-f3' : 'Заголовок шрифта:',
+      'fae_label-color-f4' : 'Код шрифта:',
+
+      fae_save_colors : 'Обновить цвета',
+      fae_default_colors : 'Вернуться к цветам по умолчанию',
+
+      confirm_update : 'Вы действительно хотите удалить свои собственные настройки цвета и вернуться к цветам форума по умолчанию?',
+      updating : 'Обновление цвета форума...',
+      updated : 'Цвета форума были успешно обновлены!',
+      reverting : 'Возврат к настройкам цвета по умолчанию...',
+      reverted : 'Цвета форума были возвращены к настройкам по умолчанию!'
+    },
+
+
+    theme_management : {
+      title : 'Управление темами',
+      fae_theme_desc : 'В этом разделе вы можете управлять темами и цветами по умолчанию для Forumactif Edge, импортируя новую тему из Github.',
+
+      'fae_label-theme' : 'Выберите тему:',
+      'fae_theme_tip-theme' : 'Выберите тип темы, которую вы хотите импортировать.',
+
+      'fae_label-color' : 'Выберите цвет:',
+      'fae_theme_tip-color' : 'Выберите цвет по умолчанию для выбранной темы.',
+
+      'fae_label-minify' : 'Уменьшить размер таблицы стилей:',
+      'fae_theme_tip-minify' : 'Сокращенные таблицы стилей занимают меньше места, но не идеально подходят для редактирования. Если вы хотите отредактировать таблицу стилей выбранной темы, выберите «Нет».',
+
+      'fae_label-rtl' : 'Справа налево:',
+      'fae_theme_tip-rtl' : 'Изменяет направление текста темы слева направо (ltr) вправо-налево (rtl) для rtl-скриптов.',
+
+      fae_import_theme : 'Тема импорта',
+      fae_import_confirm : 'Вы действительно хотите импортировать тему «{THEME_NAME}»?\\\nНе забудьте сохранить текущую таблицу стилей, если Вы хотите сохранить ее, потому что она будет изменена при импорте новой темы. Выберите «Отмена», если вы не готовы импортировать новую тему.'
+    },
+
+
+    plugin_management : {
+      title : 'Управление плагинами',
+      fae_plugin_desc : 'Этот раздел позволяет вам управлять настройками core-плагинов для форума.',
+
+      'fae_label-qnp' : 'Быстрая навигация:',
+      'fae_plugin_tip-qnp' : 'Позиция меню быстрой навигации.',
+
+      'fae_label-qns' : 'Всегда показывать быструю навигацию:',
+      'fae_plugin_tip-qns' : 'По умолчанию быстрая навигация показывается только тогда, когда навигационная панель находится вне зоны видимости. Если включить эту опцию, быстрая навигация всегда будет видимой.',
+
+      'fae_label-tso' : 'Опции выбора тем:',
+      'fae_plugin_tip-tso' : 'Этот инструмент позволяет Вам редактировать список выбора тем, предоставляя вам возможность добавлять, удалять и редактировать темы.',
+
+      fae_themer_add : 'Новая тема',
+      fae_themer_import : 'Импортировать по умолчанию',
+      fae_import_confirm : 'Вы хотите импортировать список тем по умолчанию из Github?',
+      fae_importing : 'Соединение с Github, пожалуйста, подождите...',
+      fae_compiling : 'Компиляция тем, подождите...',
+
+      fae_save_plugins : 'Обновление плагинов',
+      fae_finding_plugins : 'Поиск [FA EDGE] ALL.JS...',
+      fae_plugins_found : '[FA EDGE] ALL.JS найден!',
+      fae_getting_plugins : 'Получение [FA EDGE] ALL.JS...',
+      fae_updating_plugins : 'Обновление Ваших плагинов...',
+      fae_plugins_updated : 'Плагины успешно обновлены!'
+    }
+
+  }
+
+};

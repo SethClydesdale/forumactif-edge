a295c70ef2a5b28fc2a1d1e105a4ca30f4c1b5d7
Seth Clydesdale <sethc1995@gmail.com>
2021-03-12 12:59:52 -0500

    prep v1.3.3 update file
    - prep v1.3.3 for testing

---
+++ updates/1.3.3.js
@@ -0,0 +1,38 @@
+/*
+
+  # Changes in v1.3.3
+
+  - fixed display of the Twemoji button in the editor.
+
+*/
+
+FAE.update_tag = 'https://github.com/SethClydesdale/forumactif-edge/releases/tag/v1.3.3';
+
+FAE.update_step = [
+  {
+    info : 'Getting forum stylesheet',
+    type : 'GET',
+     url : '/admin/index.forum?mode=colors&part=themes&sub=logos&tid=' + FAE.tid,
+    func : function(d) {
+      var form = $('form[method="post"]', d)[0];
+
+      if (form) {
+        FAE.step[FAE.index + 1].data = {
+                edit_code : form.edit_code.value
+                            /*new css*/
+                            '\n/* added in FAE v1.3.3 */\n.sceditor-button-twemojifa:after,.sceditor-button-twemojifa:before{content:""}',
+                   submit : 'Submit'
+        };
+      }
+
+    }
+  },
+
+
+  {
+    info : 'Patching in updated styles',
+    type : 'POST',
+     url : 'part=themes&sub=logos&mode=css&extended_admin=1',
+    data : {}
+  }
+];
